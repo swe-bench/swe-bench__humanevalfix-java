b78f0163369bbf6b0ca37649eb91bfb29e0595da
swe-bench <>
2024-06-12 16:15:32 -0400

    Task instance Java/88

--- Main.java
+++ Main.java
@@ -3,50 +3,41 @@ import java.lang.*;
 
 class Solution {
     /**
-    You are given a 2 dimensional data, as a nested lists,
-    which is similar to matrix, however, unlike matrices,
-    each row may contain a different number of columns.
-    Given lst, and integer x, find integers x in the list,
-    and return list of lists, [[x1, y1], [x2, y2] ...] such that
-    each list is a coordinate - (row, columns), starting with 0.
-    Sort coordinates initially by rows in ascending order.
-    Also, sort coordinates of the row by columns in descending order.
+    Given an array of non-negative integers, return a copy of the given array after sorting,
+    you will sort the given array in ascending order if the sum( first index value, last index value) is odd,
+    or sort it in descending order if the sum( first index value, last index value) is even.
+
+    Note:
+    * don't change the given array.
 
     Examples:
-    getRow([
-      [1,2,3,4,5,6],
-      [1,2,3,4,1,6],
-      [1,2,3,4,5,1]
-    ], 1) == [[0, 0], [1, 4], [1, 0], [2, 5], [2, 0]]
-    getRow([], 1) == []
-    getRow([[], [1], [1, 2, 3]], 3) == [[2, 2]]
+    * sortArray(Arrays.asList()) => []
+    * sortArray(Arrays.asList(5)) => [5]
+    * sortArray(Arrays.asList(2, 4, 3, 0, 1, 5)) => [0, 1, 2, 3, 4, 5]
+    * sortArray(Arrays.asList(2, 4, 3, 0, 1, 5, 6)) => [6, 5, 4, 3, 2, 1, 0]
      */
-    public List<List<Integer>> getRow(List<List<Integer>> lst, int x) {
+    public List<Integer> sortArray(List<Integer> array) {
 
-        List<List<Integer>> coords = new ArrayList<>();
-        for (int i = 0; i < lst.size(); i++) {
-            List<List<Integer>> row = new ArrayList<>();
-            for (int j = lst.get(i).size() - 1; j >= 0; j--) {
-                if (lst.get(i).get(j) == x) {
-                    row.add(Arrays.asList(j, i));
-                }
-            }
-            coords.addAll(row);
+        if (array.size() == 0) {
+            return array;
         }
-        return coords;
+        List<Integer> result = new ArrayList<>(array);
+        if ((result.get(0) + result.get(result.size() - 1)) % 2 != 1) {
+            Collections.sort(result);
+        } else {
+            result.sort(Collections.reverseOrder());
+        }
+        return result;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.getRow(Arrays.asList(
-                        Arrays.asList(1, 2, 3, 4, 5, 6),
-                        Arrays.asList(1, 2, 3, 4, 1, 6),
-                        Arrays.asList(1, 2, 3, 4, 5, 1)
-                ), 1).equals(Arrays.asList(Arrays.asList(0, 0), Arrays.asList(1, 4), Arrays.asList(1, 0), Arrays.asList(2, 5), Arrays.asList(2, 0))),
-                s.getRow(List.of(), 1).equals(List.of()),
-                s.getRow(Arrays.asList(List.of(), List.of(1), Arrays.asList(1, 2, 3)), 3).equals(List.of(Arrays.asList(2, 2)))
+                s.sortArray(new ArrayList<>(List.of())).equals(List.of()),
+                s.sortArray(new ArrayList<>(List.of(5))).equals(List.of(5)),
+                s.sortArray(new ArrayList<>(Arrays.asList(2, 4, 3, 0, 1, 5))).equals(Arrays.asList(0, 1, 2, 3, 4, 5)),
+                s.sortArray(new ArrayList<>(Arrays.asList(2, 4, 3, 0, 1, 5, 6))).equals(Arrays.asList(6, 5, 4, 3, 2, 1, 0))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
