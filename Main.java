d8e1872c2e7b333a75dbdf78937259a55ba16b33
swe-bench <>
2024-06-12 16:15:29 -0400

    Task instance Java/50

--- Main.java
+++ Main.java
@@ -3,39 +3,25 @@ import java.lang.*;
 
 class Solution {
     /**
-    Return 2^n modulo p (be aware of numerics).
-    >>> modp(3, 5)
-    3
-    >>> modp(1101, 101)
-    2
-    >>> modp(0, 101)
-    1
-    >>> modp(3, 11)
-    8
-    >>> modp(100, 101)
-    1
+    returns encoded string by shifting every character by 5 in the alphabet.
      */
-    public int modp(int n, int p) {
-
-        int ret = 0;
-        for (int i = 0; i < n; i++) {
-            ret = (ret * 2) % p;
+    public String encodeShift(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            sb.append((char) ('a' + ((ch + 5 - 'a') % 26)));
         }
-        return ret;
+        return sb.toString();
     }
-}
-public class Main {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        List<Boolean> correct = Arrays.asList(
-                s.modp(3, 5) == 3,
-                s.modp(1101, 101) == 2,
-                s.modp(0, 101) == 1,
-                s.modp(3, 11) == 8,
-                s.modp(100, 101) == 1
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
+
+    /**
+    takes as input string encoded with encodeShift function. Returns decoded string.
+     */
+    public String decodeShift(String s) {
+
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            sb.append((char) ('a' + ((ch + 21 - ch) % 26)));
         }
+        return sb.toString();
     }
 }
