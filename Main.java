b22b92ee4291f86045f5f94a63b6d805e53caf33
swe-bench <>
2024-06-12 16:15:35 -0400

    Task instance Java/123

--- Main.java
+++ Main.java
@@ -3,29 +3,46 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given a non-empty array of integers arr and an integer k, return
-    the sum of the elements with at most two digits from the first k elements of arr.
+    Given a positive integer n, return a sorted list that has the odd numbers in collatz sequence.
 
-    Example:
+    The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined
+    as follows: start with any positive integer n. Then each term is obtained from the
+    previous term as follows: if the previous term is even, the next term is one half of
+    the previous term. If the previous term is odd, the next term is 3 times the previous
+    term plus 1. The conjecture is that no matter what value of n, the sequence will always reach 1.
 
-        Input: arr = [111,21,3,4000,5,6,7,8,9], k = 4
-        Output: 24 # sum of 21 + 3
+    Note:
+        1. Collatz(1) is [1].
+        2. returned list sorted in increasing order.
 
-    Constraints:
-        1. 1 <= len(arr) <= 100
-        2. 1 <= k <= len(arr)
+    For example:
+    getOddCollatz(5) returns [1, 5] # The collatz sequence for 5 is [5, 16, 8, 4, 2, 1], so the odd numbers are only 1, and 5.
      */
-    public int addElements(List<Integer> arr, int k) {
+    public List<Integer> getOddCollatz(int n) {
 
-        Optional<Integer> sum = arr.stream().filter(p -> String.valueOf(Math.abs(p)).length() <= 2).reduce(Integer::sum);
-        return sum.orElse(0);
+        List<Integer> odd_collatz = new ArrayList<>();
+        if (n % 2 == 1) {
+            odd_collatz.add(n);
+        }
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n * 2 + 1;
+            }
+            if (n % 2 == 1) {
+                odd_collatz.add(n);
+            }
+        }
+        Collections.sort(odd_collatz);
+        return odd_collatz;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.addElements(Arrays.asList(111, 21, 3, 4000, 5, 6, 7, 8, 9), 4) == 24
+                s.getOddCollatz(5).equals(Arrays.asList(1, 5))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
