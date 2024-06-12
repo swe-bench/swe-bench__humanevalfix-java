2e8cd73cfcc062f1a46f276be9221cfc0ba94f3b
swe-bench <>
2024-06-12 16:15:37 -0400

    Task instance Java/146

--- Main.java
+++ Main.java
@@ -3,49 +3,34 @@ import java.lang.*;
 
 class Solution {
     /**
-    Write a function which sorts the given list of integers
-    in ascending order according to the sum of their digits.
-    Note: if there are several items with similar sum of their digits,
-    order them based on their index in original list.
-
+    Write a function that takes an array of numbers as input and returns
+    the number of elements in the array that are greater than 10 and both
+    first and last digits of a number are odd (1, 3, 5, 7, 9).
     For example:
-    >>> orderByPoints(Arrays.asList(1, 11, -1, -11, -12)) == [-1, -11, 1, -12, 11]
-    >>> orderByPoints(Arrays.asList()) == []
+    specialFilter(Arrays.asList(15, -73, 14, -15)) => 1
+    specialFilter(Arrays.asList(33, -2, -3, 45, 21, 109)) => 2
      */
-    public List<Integer> orderByPoints(List<Integer> nums) {
+    public int specialFilter(List<Integer> nums) {
 
-        List<Integer> result = new ArrayList<>(nums);
-        result.sort((o1, o2) -> {
-            int sum1 = 0;
-            int sum2 = 0;
-
-            for (int i = 0; i < String.valueOf(o1).length(); i++) {
-                if (i != 0 || o1 >= 0) {
-                    sum1 += (String.valueOf(o1).charAt(i) - '0' );
-                    if (i == 1 && o1 < 0) {
-                        sum1 = -sum1;
-                    }
+        int count = 0;
+        for (int num : nums) {
+            if (num > 10) {
+                String odd_digits = "123579";
+                String number_as_string = String.valueOf(num);
+                if (odd_digits.indexOf(number_as_string.charAt(0)) != -1 && odd_digits.indexOf(number_as_string.charAt(number_as_string.length() - 1)) != -1) {
+                    count += 1;
                 }
             }
-            for (int i = 0; i < String.valueOf(o2).length(); i++) {
-                if (i != 0 || o2 >= 0) {
-                    sum2 += (String.valueOf(o2).charAt(i) - '0' );
-                    if (i == 1 && o2 < 0) {
-                        sum2 = -sum2 + sum1;
-                    }
-                }
-            }
-            return Integer.compare(sum1, sum2);
-        });
-        return result;
+        }
+        return count;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.orderByPoints(new ArrayList<>(Arrays.asList(1, 11, -1, -11, -12))).equals(Arrays.asList(-1, -11, 1, -12, 11)),
-                s.orderByPoints(new ArrayList<>(List.of())).equals(List.of())
+                s.specialFilter(Arrays.asList(15, -73, 14, -15)) == 1,
+                s.specialFilter(Arrays.asList(33, -2, -3, 45, 21, 109)) == 2
         );
         if (correct.contains(false)) {
             throw new AssertionError();
