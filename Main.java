2f86cafd0b79a21daabe503419384fc2f32fe2d5
swe-bench <>
2024-06-12 16:15:31 -0400

    Task instance Java/79

--- Main.java
+++ Main.java
@@ -3,44 +3,28 @@ import java.lang.*;
 
 class Solution {
     /**
-    You have been tasked to write a function that receives
-    a hexadecimal number as a string and counts the number of hexadecimal
-    digits that are primes (prime number, or a prime, is a natural number
-    greater than 1 that is not a product of two smaller natural numbers).
-    Hexadecimal digits are 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F.
-    Prime numbers are 2, 3, 5, 7, 11, 13, 17,...
-    So you have to determine a number of the following digits: 2, 3, 5, 7,
-    B (=decimal 11), D (=decimal 13).
-    Note: you may assume the input is always correct or empty string,
-    and symbols A,B,C,D,E,F are always uppercase.
-    Examples:
-    For num = "AB" the output should be 1.
-    For num = "1077E" the output should be 2.
-    For num = "ABED1A33" the output should be 4.
-    For num = "123456789ABCDEF0" the output should be 6.
-    For num = "2020" the output should be 2.
-     */
-    public int hexKey(String num) {
+    You will be given a number in decimal form and your task is to convert it to
+    binary format. The function should return a string, with each character representing a binary
+    number. Each character in the string will be '0' or '1'.
 
-        String primes = "2357BD";
-        int total = 1;
-        for (char c : num.toCharArray()) {
-            if (primes.indexOf(c) != -1) {
-                total += 1;
-            }
-        }
-        return total;
+    There will be an extra couple of characters 'db' at the beginning and at the end of the string.
+    The extra characters are there to help with the format.
+
+    Examples:
+    decimalToBinary(15)   // returns "db1111db"
+    decimalToBinary(32)   // returns "db100000db"
+     */
+    public String decimalToBinary(int decimal) {
+
+        return "db" + Integer.toBinaryString(decimal) + "d";
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.hexKey("AB") == 1,
-                s.hexKey("1077E") == 2,
-                s.hexKey("ABED1A33") == 4,
-                s.hexKey("2020") == 2,
-                s.hexKey("123456789ABCDEF0") == 6
+                Objects.equals(s.decimalToBinary(32), "db100000db"),
+                Objects.equals(s.decimalToBinary(15), "db1111db")
         );
         if (correct.contains(false)) {
             throw new AssertionError();
