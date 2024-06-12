098a647cdc85bb179ed72e894ba8b443e021b857
swe-bench <>
2024-06-12 16:15:28 -0400

    Task instance Java/29

--- Main.java
+++ Main.java
@@ -1,25 +1,26 @@
 import java.util.*;
 import java.lang.*;
+import java.util.stream.Collectors;
 
 class Solution {
     /**
-    Concatenate list of strings into a single string
-    >>> concatenate(List.of())
-    ""
-    >>> concatenate(Arrays.asList("a", "b", "c"))
-    "abc"
+    Filter an input list of strings only for ones that start with a given prefix.
+    >>> filterByPrefix(List.of(), "a")
+    []
+    >>> filterByPrefix(Arrays.asList("abc", "bcd", "cde", "array"), "a")
+    ["abc", "array"]
      */
-    public String concatenate(List<String> strings) {
+    public List<String> filterByPrefix(List<String> strings, String prefix) {
 
-        return String.join(" ", strings);
+        return strings.stream().filter(p -> p.endsWith(prefix)).collect(Collectors.toList());
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                Objects.equals(s.concatenate(new ArrayList<>(List.of())), ""),
-                Objects.equals(s.concatenate(new ArrayList<>(Arrays.asList("a", "b", "c"))),"abc")
+                s.filterByPrefix(new ArrayList<>(List.of()), "a").equals(List.of()),
+                s.filterByPrefix(new ArrayList<>(Arrays.asList("abc", "bcd", "cde", "array")), "a").equals(Arrays.asList("abc", "array"))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
