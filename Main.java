4e1aa7f3437025da7f19d5e63414c6f6093caf8c
swe-bench <>
2024-06-12 16:15:30 -0400

    Task instance Java/70

--- Main.java
+++ Main.java
@@ -3,37 +3,39 @@ import java.lang.*;
 
 class Solution {
     /**
-    You are given a non-empty list of positive integers. Return the greatest integer that is greater than
-    zero, and has a frequency greater than or equal to the value of the integer itself.
-    The frequency of an integer is the number of times it appears in the list.
-    If no such a value exist, return -1.
-    Examples:
-        search(Arrays.asList(4, 1, 2, 2, 3, 1)) == 2
-        search(Arrays.asList(1, 2, 2, 3, 3, 3, 4, 4, 4)) == 3
-        search(Arrays.asList(5, 5, 4, 4, 4)) == -1
-     */
-    public int search(List<Integer> lst) {
+    Given list of integers, return list in strange order.
+    Strange sorting, is when you start with the minimum value,
+    then maximum of the remaining integers, then minimum and so on.
 
-        int[] frq = new int[Collections.max(lst) + 1];
-        for (int i : lst) {
-            frq[i] += 1;
-        }
-        int ans = 0;
-        for (int i = 1; i < frq.length; i++) {
-            if (frq[i] >= i) {
-                ans = i;
+    Examples:
+    strangeSortList(Arrays.asList(1, 2, 3, 4)) == Arrays.asList(1, 4, 2, 3)
+    strangeSortList(Arrays.asList(5, 5, 5, 5)) == Arrays.asList(5, 5, 5, 5)
+    strangeSortList(Arrays.asList()) == Arrays.asList()
+     */
+    public List<Integer> strangeSortList(List<Integer> lst) {
+
+        List<Integer> res = new ArrayList<>();
+        boolean _switch = false;
+        List<Integer> l = new ArrayList<>(lst);
+        while (l.size() != 0) {
+            if (_switch) {
+                res.add(Collections.min(l));
+            } else {
+                res.add(Collections.max(l));
             }
+            l.remove(res.get(res.size() - 1));
+            _switch = !_switch;
         }
-        return ans;
+        return res;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.search(new ArrayList<>(Arrays.asList(4, 1, 2, 2, 3, 1))) == 2,
-                s.search(new ArrayList<>(Arrays.asList(1, 2, 2, 3, 3, 3, 4, 4, 4))) == 3,
-                s.search(new ArrayList<>(Arrays.asList(5, 5, 4, 4, 4))) == -1
+                s.strangeSortList(new ArrayList<>(Arrays.asList(1, 2, 3, 4))).equals(Arrays.asList(1, 4, 2, 3)),
+                s.strangeSortList(new ArrayList<>(Arrays.asList(5, 5, 5, 5))).equals(Arrays.asList(5, 5, 5, 5)),
+                s.strangeSortList(new ArrayList<>(List.of())).equals(List.of())
         );
         if (correct.contains(false)) {
             throw new AssertionError();
