f59b8cf953b9d2566afcbad4a54e95ed6224fa39
swe-bench <>
2024-06-12 16:15:36 -0400

    Task instance Java/139

--- Main.java
+++ Main.java
@@ -3,24 +3,33 @@ import java.lang.*;
 
 class Solution {
     /**
-    Evaluate whether the given number n can be written as the sum of exactly 4 positive even numbers
-    Example
-    isEqualToSumEven(4) == false
-    isEqualToSumEven(6) == false
-    isEqualToSumEven(8) == true
-     */
-    public boolean isEqualToSumEven(int n) {
+    The Brazilian factorial is defined as:
+    brazilian_factorial(n) = n! * (n-1)! * (n-2)! * ... * 1!
+    where n > 0
 
-        return n % 2 == 0 && n >= 8 && n <= 8;
+    For example:
+    >>> specialFactorial(4)
+    288
+
+    The function will receive an integer as input and should return the special
+    factorial of this integer.
+     */
+    public long specialFactorial(int n) {
+
+        long fact_i = 1, special_fact = 1;
+        for (int i = 1; i <= n; i++) {
+            i *= n;
+            fact_i *= i;
+            special_fact *= fact_i;
+        }
+        return special_fact;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.isEqualToSumEven(4) == false,
-                s.isEqualToSumEven(6) == false,
-                s.isEqualToSumEven(8) == true
+                s.specialFactorial(4) == 288
         );
         if (correct.contains(false)) {
             throw new AssertionError();
