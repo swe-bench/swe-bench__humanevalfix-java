eab756ca8f85425f6124225287fbf77ad068df63
swe-bench <>
2024-06-12 16:15:34 -0400

    Task instance Java/110

--- Main.java
+++ Main.java
@@ -3,57 +3,43 @@ import java.lang.*;
 
 class Solution {
     /**
-    We have an array 'arr' of N integers arr[1], arr[2], ..., arr[N].The
-    numbers in the array will be randomly ordered. Your task is to determine if
-    it is possible to get an array sorted in non-decreasing order by performing
-    the following operation on the given array:
-        You are allowed to perform right shift operation any number of times.
-
-    One right shift operation means shifting all elements of the array by one
-    position in the right direction. The last element of the array will be moved to
-    the starting position in the array i.e. 0th index.
-
-    If it is possible to obtain the sorted array by performing the above operation
-    then return true else return False.
-    If the given array is empty then return true.
-
-    Note: The given list is guaranteed to have unique elements.
-
-    For Example:
-
-    moveOneBall(Arrays.asList(3, 4, 5, 1, 2))==>true
-    Explanation: By performin 2 right shift operations, non-decreasing order can
-                 be achieved for the given array.
-    moveOneBall(Arrays.asList(3, 5, 4, 1, 2))==>False
-    Explanation:It is not possible to get non-decreasing order for the given
-                array by performing any number of right shift operations.
+    In this problem, you will implement a function that takes two lists of numbers,
+    and determines whether it is possible to perform an exchange of elements
+    between them to make lst1 a list of only even numbers.
+    There is no limit on the number of exchanged elements between lst1 and lst2.
+    If it is possible to exchange elements between the lst1 and lst2 to make
+    all the elements of lst1 to be even, return "YES".
+    Otherwise, return "NO".
+    For example:
+    exchange(Arrays.asList(1, 2, 3, 4), Arrays.asList(1, 2, 3, 4)) => "YES"
+    exchange(Arrays.asList(1, 2, 3, 4), Arrays.asList(1, 5, 3, 4)) => "NO"
+    It is assumed that the input lists will be non-empty.
      */
-    public boolean moveOneBall(List<Integer> arr) {
+    public String exchange(List<Integer> lst1, List<Integer> lst2) {
 
-        if (arr.size() == 0) {
-            return true;
-        }
-        List<Integer> sorted_arr = new ArrayList<>(arr);
-        Collections.sort(sorted_arr);
-
-        int min_value = Collections.min(arr);
-        int min_index = sorted_arr.indexOf(min_value);
-        List<Integer> my_arr = new ArrayList<>(arr.subList(min_index, arr.size()));
-        my_arr.addAll(arr.subList(0, min_index));
-        for (int i = 0; i < arr.size(); i++) {
-            if (my_arr.get(i) != sorted_arr.get(i)) {
-                return false;
+        int odd = 0, even = 0;
+        for (int i : lst1) {
+            if (i % 2 == 1) {
+                odd += 1;
             }
         }
-        return true;
+        for (int i : lst2) {
+            if (i % 2 == 0) {
+                even += 1;
+            }
+        }
+        if (odd >= even) {
+            return "YES";
+        }
+        return "NO";
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.moveOneBall(new ArrayList<>(Arrays.asList(3, 4, 5, 1, 2))) == true,
-                s.moveOneBall(new ArrayList<>(Arrays.asList(3, 5, 4, 1, 2))) == false
+                Objects.equals(s.exchange(Arrays.asList(1, 2, 3, 4), Arrays.asList(1, 2, 3, 4)), "YES" ),
+                Objects.equals(s.exchange(Arrays.asList(1, 2, 3, 4), Arrays.asList(1, 5, 3, 4)), "NO" )
         );
         if (correct.contains(false)) {
             throw new AssertionError();
