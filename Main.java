5948ab705949e35184b51690fc831c896422d018
swe-bench <>
2024-06-12 16:15:37 -0400

    Task instance Java/158

--- Main.java
+++ Main.java
@@ -3,25 +3,46 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given the lengths of the three sides of a triangle. Return true if the three
-    sides form a right-angled triangle, false otherwise.
-    A right-angled triangle is a triangle in which one angle is right angle or
-    90 degree.
-    Example:
-    rightAngleTriangle(3, 4, 5) == true
-    rightAngleTriangle(1, 2, 3) == false
-     */
-    public boolean rightAngleTriangle(int a, int b, int c) {
+    Write a function that accepts a list of strings.
+    The list contains different words. Return the word with maximum number
+    of unique characters. If multiple strings have maximum number of unique
+    characters, return the one which comes first in lexicographical order.
 
-        return c * c == a * a + b * b;
+    findMax(["name", "of", "string"]) == "string"
+    findMax(["name", "enam", "game"]) == "enam"
+    findMax(["aaaaaaa", "bb" ,"cc"]) == ""aaaaaaa"
+     */
+    public String findMax(List<String> words) {
+
+        List<String> words_sort = new ArrayList<>(words);
+        words_sort.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                Set<Character> s1 = new HashSet<>();
+                for (char ch : o1.toCharArray()) {
+                    s1.add(ch);
+                }
+                Set<Character> s2 = new HashSet<>();
+                for (char ch : o2.toCharArray()) {
+                    s2.add(ch);
+                }
+                if (s1.size() > s2.size()) {
+                    return 1;
+                } else {
+                    return -o1.compareTo(o2);
+                }
+            }
+        });
+        return words_sort.get(words_sort.size() - 1);
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.rightAngleTriangle(3, 4, 5) == true,
-                s.rightAngleTriangle(1, 2, 3) == false
+                s.findMax(new ArrayList<>(Arrays.asList("name", "of", "string"))).equals("string"),
+                s.findMax(new ArrayList<>(Arrays.asList("name", "enam", "game"))).equals("enam"),
+                s.findMax(new ArrayList<>(Arrays.asList("aaaaaaa", "bb", "cc"))).equals("aaaaaaa")
         );
         if (correct.contains(false)) {
             throw new AssertionError();
