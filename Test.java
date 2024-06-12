3ea0c2c8efc6ac8689b56518950c8fe42c4b8305
swe-bench <>
2024-06-12 16:15:27 -0400

    Task instance Java/18 with tests

---
+++ Test.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+import java.security.*;
+import java.util.*;
+import java.lang.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<Boolean> correct = Arrays.asList(
+                s.howManyTimes("", "x") == 0,
+                s.howManyTimes("xyxyxyx", "x") == 4,
+                s.howManyTimes("cacacacac", "cac") == 4,
+                s.howManyTimes("john doe", "john") == 1
+        );
+        if (correct.contains(false)) {
+            throw new AssertionError();
+        }
+    }
+}
