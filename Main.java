e18d1d2773271f9e483dc74ba779d6d3e0ae8bdd
swe-bench <>
2024-06-12 16:15:35 -0400

    Task instance Java/124

--- Main.java
+++ Main.java
@@ -3,46 +3,66 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given a positive integer n, return a sorted list that has the odd numbers in collatz sequence.
+    You have to write a function which validates a given date string and
+    returns true if the date is valid otherwise false.
+    The date is valid if all of the following rules are satisfied:
+    1. The date string is not empty.
+    2. The number of days is not less than 1 or higher than 31 days for months 1,3,5,7,8,10,12. And the number of days is not less than 1 or higher than 30 days for months 4,6,9,11. And, the number of days is not less than 1 or higher than 29 for the month 2.
+    3. The months should not be less than 1 or higher than 12.
+    4. The date should be in the format: mm-dd-yyyy
 
-    The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined
-    as follows: start with any positive integer n. Then each term is obtained from the
-    previous term as follows: if the previous term is even, the next term is one half of
-    the previous term. If the previous term is odd, the next term is 3 times the previous
-    term plus 1. The conjecture is that no matter what value of n, the sequence will always reach 1.
-
-    Note:
-        1. Collatz(1) is [1].
-        2. returned list sorted in increasing order.
-
-    For example:
-    getOddCollatz(5) returns [1, 5] # The collatz sequence for 5 is [5, 16, 8, 4, 2, 1], so the odd numbers are only 1, and 5.
+    for example:
+    validDate("03-11-2000") => true
+    validDate("15-01-2012") => false
+    validDate("04-0-2040") => false
+    validDate("06-04-2020") => true
+    validDate("06/04/2020") => false
      */
-    public List<Integer> getOddCollatz(int n) {
+    public boolean validDate(String date) {
 
-        List<Integer> odd_collatz = new ArrayList<>();
-        if (n % 2 == 1) {
-            odd_collatz.add(n);
-        }
-        while (n > 1) {
-            if (n % 2 == 0) {
-                n = n / 2;
-            } else {
-                n = n * 2 + 1;
+        try {
+            date = date.strip();
+            String[] dates = date.split("-" );
+            String m = dates[1];
+            while (!m.isEmpty() && m.charAt(0) == '0') {
+                m = m.substring(1);
             }
-            if (n % 2 == 1) {
-                odd_collatz.add(n);
+            String d = dates[0];
+            while (!d.isEmpty() && d.charAt(0) == '0') {
+                d = d.substring(1);
             }
+            String y = dates[2];
+            while (!y.isEmpty() && y.charAt(0) == '0') {
+                y = y.substring(1);
+            }
+            int month = Integer.parseInt(m), day = Integer.parseInt(d), year = Integer.parseInt(y);
+            if (month < 1 || month > 12) {
+                return false;
+            }
+            if (Arrays.asList(1, 3, 5, 7, 8, 10, 12).contains(month) && (day < 1 || day > 31)) {
+                return false;
+            }
+            if (Arrays.asList(4, 6, 9, 11).contains(month) && (day < 1 || day > 30)) {
+                return false;
+            }
+            if (month == 2 && (day < 1 || day > 29)) {
+                return false;
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        Collections.sort(odd_collatz);
-        return odd_collatz;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.getOddCollatz(5).equals(Arrays.asList(1, 5))
+                s.validDate("03-11-2000" ) == true,
+                s.validDate("15-01-2012" ) == false,
+                s.validDate("04-0-2040" ) == false,
+                s.validDate("06-04-2020" ) == true,
+                s.validDate("06/04/2020" ) == false
         );
         if (correct.contains(false)) {
             throw new AssertionError();
