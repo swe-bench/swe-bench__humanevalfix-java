f549d5c96ecf711347f325dbd5db3eb025e824a0
swe-bench <>
2024-06-12 16:15:34 -0400

    Task instance Java/107

--- Main.java
+++ Main.java
@@ -3,40 +3,49 @@ import java.lang.*;
 
 class Solution {
     /**
-    Implement the function f that takes n as a parameter,
-    and returns a list of size n, such that the value of the element at index i is the factorial of i if i is even
-    or the sum of numbers from 1 to i otherwise.
-    i starts from 1.
-    the factorial of i is the multiplication of the numbers from 1 to i (1 * 2 * ... * i).
-    Example:
-    f(5) == [1, 2, 6, 24, 15]
-     */
-    public List<Integer> f(int n) {
+    Given a positive integer n, return a tuple that has the number of even and odd
+    integer palindromes that fall within the range(1, n), inclusive.
 
-        List<Integer> ret = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                int x = 1;
-                for (int j = 1; j <= i; j++) {
-                    x *= i;
+    Example 1:
+
+        Input: 3
+        Output: (1, 2)
+        Explanation:
+        Integer palindrome are 1, 2, 3. one of them is even, and two of them are odd.
+
+    Example 2:
+
+        Input: 12
+        Output: (4, 6)
+        Explanation:
+        Integer palindrome are 1, 2, 3, 4, 5, 6, 7, 8, 9, 11. four of them are even, and 6 of them are odd.
+
+    Note:
+        1. 1 <= n <= 10^3
+        2. returned tuple has the number of even and odd integer palindromes respectively.
+     */
+    public List<Integer> evenOddPalindrome(int n) {
+
+        int even_palindrome_count = 0, odd_palindrome_count = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (String.valueOf(i).equals(new StringBuilder(String.valueOf(i)).reverse().toString())) {
+                if (i % 2 == 1) {
+                    odd_palindrome_count += 1;
+                } else {
+                    even_palindrome_count += 1;
                 }
-                ret.add(x);
-            } else {
-                int x = 0;
-                for (int j = 1; j <= i; j++) {
-                    x += j;
-                }
-                ret.add(x);
             }
         }
-        return ret;
+        return Arrays.asList(even_palindrome_count, odd_palindrome_count);
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.f(5).equals(Arrays.asList(1, 2, 6, 24, 15))
+                s.evenOddPalindrome(12).equals(Arrays.asList(4, 6)),
+                s.evenOddPalindrome(3).equals(Arrays.asList(1, 2))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
