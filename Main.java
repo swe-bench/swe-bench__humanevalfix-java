03af1b2f832af3f9c4644c897e2243cca8ba95b7
swe-bench <>
2024-06-12 16:15:31 -0400

    Task instance Java/76

--- Main.java
+++ Main.java
@@ -3,51 +3,39 @@ import java.lang.*;
 
 class Solution {
     /**
-    Write a function that returns true if the given number is the multiplication of 3 prime numbers
-    and false otherwise.
-    Knowing that (a) is less then 100.
-    Example:
-    isMultiplyPrime(30) == true
-    30 = 2 * 3 * 5
+    Your task is to write a function that returns true if a number x is a simple
+    power of n and false in other cases.
+    x is a simple power of n if n**int=x
+    For example:
+    isSimplePower(1, 4) => true
+    isSimplePower(2, 2) => true
+    isSimplePower(8, 2) => true
+    isSimplePower(3, 2) => false
+    isSimplePower(3, 1) => false
+    isSimplePower(5, 3) => false
      */
-    public boolean isMultiplyPrime(int a) {
+    public boolean isSimplePower(int x, int n) {
 
-        class IsPrime {
-            public static boolean is_prime(int n) {
-                for (int j = 0; j < n; j++) {
-                    if (n % j == 0) {
-                        return false;
-                    }
-                }
-                return true;
-            }
+        if (n == 1) {
+            return x == 1;
         }
-        for (int i = 2; i < 101; i++) {
-            if (!IsPrime.is_prime(i)) {
-                continue;
-            }
-            for (int j = i; j < 101; j++) {
-                if (!IsPrime.is_prime(j)) {
-                    continue;
-                }
-                for (int k = j; k < 101; k++) {
-                    if (!IsPrime.is_prime(k)) {
-                        continue;
-                    }
-                    if (i * j * k == a) {
-                        return true;
-                    }
-                }
-            }
+        int power = 1;
+        while (n < x) {
+            power = power * n;
         }
-        return false;
+        return power == x;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.isMultiplyPrime(30)
+                s.isSimplePower(1, 4),
+                s.isSimplePower(2, 2),
+                s.isSimplePower(8, 2),
+                !s.isSimplePower(3, 2),
+                !s.isSimplePower(3, 1),
+                !s.isSimplePower(5, 3)
         );
         if (correct.contains(false)) {
             throw new AssertionError();
