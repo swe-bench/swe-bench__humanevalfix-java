7c8ffbe7cdd07cb8cf70c4f92e93bcecd9eab722
swe-bench <>
2024-06-12 16:15:33 -0400

    Task instance Java/103

--- Main.java
+++ Main.java
@@ -3,34 +3,32 @@ import java.lang.*;
 
 class Solution {
     /**
-    This function takes two positive numbers x and y and returns the
-    biggest even integer number that is in the range [x, y] inclusive. If
-    there's no such number, then the function should return -1.
-    
-    For example:
-    chooseNum(12, 15) = 14
-    chooseNum(13, 12) = -1
+    You are given two positive integers n and m, and your task is to compute the
+    average of the integers from n through m (including n and m).
+    Round the answer to the nearest integer and convert that to binary.
+    If n is greater than m, return -1.
+    Example:
+    roundedAvg(1, 5) => "11"
+    roundedAvg(7, 5) => -1
+    roundedAvg(10, 20) => "1111"
+    roundedAvg(20, 33) => "11011"
      */
-    public int chooseNum(int x, int y) {
+    public Object roundedAvg(int n, int m) {
 
-        if (x > y) {
+        if (n > m) {
             return -1;
         }
-        if (y % 2 == 0) {
-            return y;
-        }
-        if (x == y) {
-            return -1;
-        }
-        return x - 1;
+        return Integer.toBinaryString((int) Math.round((double) (m + n + 1) / 2));
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.chooseNum(12, 15) == 14,
-                s.chooseNum(13, 12) == -1
+                Objects.equals((String) s.roundedAvg(1, 5), "11" ),
+                (int) s.roundedAvg(7, 5) == -1,
+                Objects.equals((String) s.roundedAvg(10, 20), "1111" ),
+                Objects.equals((String) s.roundedAvg(20, 33), "11011" )
         );
         if (correct.contains(false)) {
             throw new AssertionError();
