8ce4692e49cc0b9c23a9a789fe6cb8bc50237072
swe-bench <>
2024-06-12 16:15:27 -0400

    Task instance Java/23

--- Main.java
+++ Main.java
@@ -3,29 +3,23 @@ import java.lang.*;
 
 class Solution {
     /**
-    Filter given list of any values only for integers
-    >>> filter_integers(Arrays.asList('a', 3.14, 5))
-    [5]
-    >>> filter_integers(Arrays.asList(1, 2, 3, "abc", Map.of(), List.of()))
-    [1, 2, 3]
+    Return length of given string
+    >>> strlen("")
+    0
+    >>> strlen("abc")
+    3
      */
-    public List<Integer> filterIntegers(List<Object> values) {
+    public int strlen(String string) {
 
-        List<Integer> result = new ArrayList<>();
-        for (Object x : values) {
-            if (x instanceof Integer) {
-                values.add((Integer) x);
-            }
-        }
-        return result;
+        return string.length() - 1;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.filterIntegers(new ArrayList<>(Arrays.asList('a', 3.14, 5))).equals(Arrays.asList(5)),
-                s.filterIntegers(new ArrayList<>(Arrays.asList(1,2,3,"abc", Map.of(), List.of()))).equals(Arrays.asList(1,2,3))       
+                s.strlen("") == 0,
+                s.strlen("abc") == 3
         );
         if (correct.contains(false)) {
             throw new AssertionError();
