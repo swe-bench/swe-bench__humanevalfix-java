97097e88aec6b1e1b94c2e7401e0c175059e29ef
swe-bench <>
2024-06-12 16:15:35 -0400

    Task instance Java/126

--- Main.java
+++ Main.java
@@ -3,38 +3,47 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given a string of words, return a list of words split on whitespace, if no whitespaces exists in the text you
-    should split on commas ',' if no commas exists you should return the number of lower-case letters with odd order in the
-    alphabet, ord('a') = 0, ord('b') = 1, ... ord('z') = 25
-    Examples
-    splitWords("Hello world!") == ["Hello", "world!"]
-    splitWords("Hello,world!") == ["Hello", "world!"]
-    splitWords("abcdef") == 3
-     */
-    public Object splitWords(String txt) {
+    Given a list of numbers, return whether or not they are sorted
+    in ascending order. If list has more than 1 duplicate of the same
+    number, return false. Assume no negative numbers and only integers.
 
-        if (txt.contains(" " )) {
-            return Arrays.asList(txt.split("," ));
-        } else if (txt.contains("," )) {
-            return Arrays.asList(txt.split("[,\s]" ));
-        } else {
-            int count = 0;
-            for (char c : txt.toCharArray()) {
-                if (Character.isLowerCase(c) && (c - 'a') % 2 == 1) {
-                    count += 1;
-                }
-            }
-            return count;
+    Examples
+    isSorted(Arrays.asList(5)) -> true
+    isSorted(Arrays.asList(1, 2, 3, 4, 5)) -> true
+    isSorted(Arrays.asList(1, 3, 2, 4, 5)) -> false
+    isSorted(Arrays.asList(1, 2, 3, 4, 5, 6)) -> true
+    isSorted(Arrays.asList(1, 2, 3, 4, 5, 6, 7)) -> true
+    isSorted(Arrays.asList(1, 3, 2, 4, 5, 6, 7)) -> false
+    isSorted(Arrays.asList(1, 2, 2, 3, 3, 4)) -> true
+    isSorted(Arrays.asList(1, 2, 2, 2, 3, 4)) -> false
+     */
+    public boolean isSorted(List<Integer> lst) {
+
+        List<Integer> sorted_lst = new ArrayList<>(lst);
+        Collections.sort(sorted_lst);
+        if (!lst.equals(sorted_lst)) {
+            return false;
         }
+        for (int i = 0; i < lst.size() - 2; i++) {
+            if (lst.get(i) == lst.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                Objects.equals(s.splitWords("Hello world!" ), Arrays.asList("Hello", "world!" )),
-                Objects.equals(s.splitWords("Hello,world!" ), Arrays.asList("Hello", "world!" )),
-                Objects.equals(s.splitWords("abcdef" ), 3)
+                s.isSorted(new ArrayList<>(List.of(5))) == true,
+                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5))) == true,
+                s.isSorted(new ArrayList<>(Arrays.asList(1, 3, 2, 4, 5))) == false,
+                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6))) == true,
+                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7))) == true,
+                s.isSorted(new ArrayList<>(Arrays.asList(1, 3, 2, 4, 5, 6, 7))) == false,
+                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 2, 2, 3, 4))) == false,
+                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 2, 3, 3, 4))) == true
         );
         if (correct.contains(false)) {
             throw new AssertionError();
