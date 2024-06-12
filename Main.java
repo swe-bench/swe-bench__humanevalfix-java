4a469d8aad63bc9dfbaecc1c246ccf8df8a6778c
swe-bench <>
2024-06-12 16:15:35 -0400

    Task instance Java/129

--- Main.java
+++ Main.java
@@ -3,35 +3,75 @@ import java.lang.*;
 
 class Solution {
     /**
-    You are given an array arr of integers and you need to return
-    sum of magnitudes of integers multiplied by product of all signs
-    of each number in the array, represented by 1, -1 or 0.
-    Note: return None for empty arr.
+    Given a grid with N rows and N columns (N >= 2) and a positive integer k,
+    each cell of the grid contains a value. Every integer in the range [1, N * N]
+    inclusive appears exactly once on the cells of the grid.
 
-    Example:
-    >>> prodSigns(Arrays.asList(1, 2, 2, -4)) == -9
-    >>> prodSigns(Arrays.asList(0, 1)) == 0
-    >>> prodSigns(Arrays.asList()) == None
+    You have to find the minimum path of length k in the grid. You can start
+    from any cell, and in each step you can move to any of the neighbor cells,
+    in other words, you can go to cells which share an edge with you current
+    cell.
+    Please note that a path of length k means visiting exactly k cells (not
+    necessarily distinct).
+    You CANNOT go off the grid.
+    A path A (of length k) is considered less than a path B (of length k) if
+    after making the ordered lists of the values on the cells that A and B go
+    through (let's call them lst_A and lst_B), lst_A is lexicographically less
+    than lst_B, in other words, there exist an integer index i (1 <= i <= k)
+    such that lst_A[i] < lst_B[i] and for any j (1 <= j < i) we have
+    lst_A[j] = lst_B[j].
+    It is guaranteed that the answer is unique.
+    Return an ordered list of the values on the cells that the minimum path go through.
+
+    Examples:
+
+        Input: grid = [ [1,2,3], [4,5,6], [7,8,9]], k = 3
+        Output: [1, 2, 1]
+
+        Input: grid = [ [5,9,3], [4,1,6], [7,8,2]], k = 1
+        Output: [1]
      */
-    public Optional<Integer> prodSigns(List<Integer> arr) {
+    public List<Integer> minPath(List<List<Integer>> grid, int k) {
 
-        if (arr.size() == 0) {
-            return Optional.empty();
+        int n = grid.size();
+        int val = n * n + 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid.get(i).get(j) == 1) {
+                    List<Integer> temp = new ArrayList<>();
+                    if (i != 0) {
+                        temp.add(grid.get(i).get(j));
+                    }
+                    if (j != 0) {
+                        temp.add(grid.get(i).get(j));
+                    }
+                    if (i != n - 1) {
+                        temp.add(grid.get(i).get(j));
+                    }
+                    if (j != n - 1) {
+                        temp.add(grid.get(i).get(j));
+                    }
+                    val = Collections.min(temp);
+                }
+            }
         }
-        if (arr.contains(0)) {
-            return Optional.of(0);
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            if (i == 0) {
+                ans.add(1);
+            } else {
+                ans.add(val);
+            }
         }
-        int prod = (int) (-2 * (arr.stream().filter(p -> p < 0).count() % 2) + 1 * 2);
-        return Optional.of(prod * (arr.stream().map(Math::abs).reduce(Integer::sum)).get());
+        return ans;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.prodSigns(Arrays.asList(1, 2, 2, -4)).get() == -9,
-                s.prodSigns(Arrays.asList(0, 1)).get() == 0,
-                s.prodSigns(List.of()).isEmpty()
+                s.minPath(Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), Arrays.asList(7, 8, 9)), 3).equals(Arrays.asList(1, 2, 1)),
+                s.minPath(Arrays.asList(Arrays.asList(5, 9, 3), Arrays.asList(4, 1, 6), Arrays.asList(7, 8, 2)), 1).equals(List.of(1))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
