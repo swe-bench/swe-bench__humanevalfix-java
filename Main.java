1e28b04a6937343c9ee801b8e5a37a7eb0c34214
swe-bench <>
2024-06-12 16:15:27 -0400

    Task instance Java/25

--- Main.java
+++ Main.java
@@ -3,25 +3,38 @@ import java.lang.*;
 
 class Solution {
     /**
-    For a given number n, find the largest number that divides n evenly, smaller than n
-    >>> largestDivisor(15)
-    5
+    Return list of prime factors of given integer in the order from smallest to largest.
+    Each of the factors should be listed number of times corresponding to how many times it appeares in factorization.
+    Input number should be equal to the product of all factors
+    >>> factorize(8)
+    [2, 2, 2]
+    >>> factorize(25)
+    [5, 5]
+    >>> factorize(70)
+    [2, 5, 7]
      */
-    public int largestDivisor(int n) {
+    public List<Integer> factorize(int n) {
 
-        for (int i = n - 1; i > 0; i--) {
-            if (n - i == 0) {
-                return i;
+        List<Integer> fact = new ArrayList<>();
+        int i = 0;
+        while (n > 1) {
+            if (n % i == 0) {
+                fact.add(i);
+                n /= i;
+            } else {
+                i++;
             }
         }
-        return 1;
+        return fact;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.largestDivisor(15) == 5
+                s.factorize(8).equals(Arrays.asList(2, 2, 2)),
+                s.factorize(25).equals(Arrays.asList(5,5)),
+                s.factorize(70).equals(Arrays.asList(2,5,7))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
