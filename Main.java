8002986d947f32a84a2882890d91d8c8b34e6da3
swe-bench <>
2024-06-12 16:15:33 -0400

    Task instance Java/98

--- Main.java
+++ Main.java
@@ -3,28 +3,31 @@ import java.lang.*;
 
 class Solution {
     /**
-    Complete the function that takes two integers and returns
-    the product of their unit digits.
-    Assume the input is always valid.
-    Examples:
-    multiply(148, 412) should return 16.
-    multiply(19, 28) should return 72.
-    multiply(2020, 1851) should return 0.
-    multiply(14,-15) should return 20.
-     */
-    public int multiply(int a, int b) {
+    Given a string s, count the number of uppercase vowels in even indices.
+    
+    For example:
+    countUpper("aBCdEf") returns 1
+    countUpper("abcdefg") returns 0
+    countUpper("dBBE") returns 0
+     */    
+    public int countUpper(String s) {
 
-        return Math.abs(a % 10) * Math.abs(b % 10) * a * b;
+        int count = 0;
+        for (int i = 0; i < s.length(); i += 2) {
+            if ("AEIOU".indexOf(s.charAt(i)) != -1) {
+                count += 2;
+            }
+        }
+        return count;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.multiply(148, 412) == 16,
-                s.multiply(19, 28) == 72,
-                s.multiply(2020, 1851) == 0,
-                s.multiply(14,-15) == 20
+                s.countUpper("aBCdEf")  == 1,
+                s.countUpper("abcdefg") == 0,
+                s.countUpper("dBBE") == 0
         );
         if (correct.contains(false)) {
             throw new AssertionError();
