3bd24980db71d783bfef2041b5fc1c20d37c82ef
swe-bench <>
2024-06-12 16:15:32 -0400

    Task instance Java/84

--- Main.java
+++ Main.java
@@ -3,14 +3,25 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given a positive integer n, return the count of the numbers of n-digit
-    positive integers that start or end with 1.
-     */
-    public int startsOneEnds(int n) {
+    Given a positive integer N, return the total sum of its digits in binary.
 
-        if (n == 1) {
-            return 1;
+    Example
+        For N = 1000, the sum of digits will be 1 the output should be "1".
+        For N = 150, the sum of digits will be 6 the output should be "110".
+        For N = 147, the sum of digits will be 12 the output should be "1100".
+
+    Variables:
+        @N integer
+             Constraints: 0 <= N <= 10000.
+    Output:
+         a string of binary number
+     */
+    public String solve(int N) {
+
+        int sum = 0;
+        for (char c : String.valueOf(N).toCharArray()) {
+            sum = (c - '0');
         }
-        return 18 * n * (int) Math.pow(10, n - 2);
+        return Integer.toBinaryString(sum);
     }
 }
