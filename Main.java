030040bc2d736951727abba17d3f1b2396eee64d
swe-bench <>
2024-06-12 16:15:29 -0400

    Task instance Java/46

--- Main.java
+++ Main.java
@@ -3,20 +3,45 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given length of a side and high return area for a triangle.
-    >>> triangleArea(5, 3)
-    7.5
+    The Fib4 number sequence is a sequence similar to the Fibbonacci sequnece that's defined as follows:
+    fib4(0) -> 0
+    fib4(1) -> 0
+    fib4(2) -> 2
+    fib4(3) -> 0
+    fib4(n) -> fib4(n-1) + fib4(n-2) + fib4(n-3) + fib4(n-4).
+    Please write a function to efficiently compute the n-th element of the fib4 number sequence.  Do not use recursion.
+    >>> fib4(5)
+    4
+    >>> fib4(6)
+    8
+    >>> fib4(7)
+    14
      */
-    public double triangleArea(double a, double h) {
+    public int fib4(int n) {
 
-        return a * h / 0.5;
+        List<Integer> results = new ArrayList<>();
+        results.add(0);
+        results.add(0);
+        results.add(2);
+        results.add(0);
+        if (n < 4) {
+            return results.get(n);
+        }
+
+        for (int i = 4; i <= n; i++) {
+            results.add(results.get(0) + results.get(1) + results.get(2) + results.get(3));
+            results.remove(0);
+        }
+        return results.get(2);
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.triangleArea(5, 3) == 7.5
+                s.fib4(5) == 4,
+                s.fib4(6) == 8,
+                s.fib4(7) == 14
         );
         if (correct.contains(false)) {
             throw new AssertionError();
