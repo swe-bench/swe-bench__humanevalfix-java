62114f9438e0cc1d6b1226eab3423b8c8d7b5494
swe-bench <>
2024-06-12 16:15:35 -0400

    Task instance Java/120

--- Main.java
+++ Main.java
@@ -3,47 +3,46 @@ import java.lang.*;
 
 class Solution {
     /**
-    You are given a list of two strings, both strings consist of open
-    parentheses "(" or close parentheses ")" only.
-    Your job is to check if it is possible to concatenate the two strings in
-    some order, that the resulting string will be good.
-    A string S is considered to be good if and only if all parentheses in S
-    are balanced. For example: the string "(())()" is good, while the string
-    "())" is not.
-    Return "Yes" if there"s a way to make a good string, and return "No" otherwise.
+    Given an array arr of integers and a positive integer k, return a sorted list
+    of length k with the maximum k numbers in arr.
 
-    Examples:
-    matchParens(Arrays.asList("()(", ")")) == "Yes"
-    matchParens(Arrays.asList(")", ")")) == "No"
+    Example 1:
+
+        Input: arr = [-3, -4, 5], k = 3
+        Output: [-4, -3, 5]
+
+    Example 2:
+
+        Input: arr = [4, -4, 4], k = 2
+        Output: [4, 4]
+
+    Example 3:
+
+        Input: arr = [-3, 2, 1, 2, -1, -2, 1], k = 1
+        Output: [2]
+
+    Note:
+        1. The length of the array will be in the range of [1, 1000].
+        2. The elements in the array will be in the range of [-1000, 1000].
+        3. 0 <= k <= len(arr)
      */
-    public String matchParens(List<String> lst) {
+    public List<Integer> maximum(List<Integer> arr, int k) {
 
-        List<String> S = Arrays.asList(lst.get(0) + lst.get(1), lst.get(1) + lst.get(0));
-        for (String s : S) {
-            int val = 0;
-            for (char i : s.toCharArray()) {
-                if (i == '(') {
-                    val += 1;
-                } else {
-                    val -= 1;
-                }
-                if (val < 0) {
-                    break;
-                }
-            }
-            if (val == 0) {
-                return "yes";
-            }
+        if (k == 0) {
+            return List.of();
         }
-        return "no";
+        List<Integer> arr_sort = new ArrayList<>(arr);
+        Collections.sort(arr_sort);
+        return arr_sort.subList(arr_sort.size() - k, arr_sort.size()).subList(0, 1);
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.matchParens(Arrays.asList("()(", ")")).equals("Yes"),
-                s.matchParens(Arrays.asList(")", ")")).equals("No")
+                s.maximum(new ArrayList<>(Arrays.asList(-3, -4, 5)), 3).equals(Arrays.asList(-4, -3, 5)),
+                s.maximum(new ArrayList<>(Arrays.asList(4, -4, 4)), 2).equals(Arrays.asList(4, 4)),
+                s.maximum(new ArrayList<>(Arrays.asList(-3, 2, 1, 2, -1, -2, 1)), 1).equals(List.of(2))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
