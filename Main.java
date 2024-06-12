521696b3a5854aa62aa0fdfd4b7197021745517e
swe-bench <>
2024-06-12 16:15:33 -0400

    Task instance Java/97

--- Main.java
+++ Main.java
@@ -3,44 +3,28 @@ import java.lang.*;
 
 class Solution {
     /**
-    Implement a function that takes an non-negative integer and returns an array of the first n
-    integers that are prime numbers and less than n.
-    for example:
-    countUpTo(5) => [2,3]
-    countUpTo(11) => [2,3,5,7]
-    countUpTo(0) => []
-    countUpTo(20) => [2,3,5,7,11,13,17,19]
-    countUpTo(1) => []
-    countUpTo(18) => [2,3,5,7,11,13,17]
+    Complete the function that takes two integers and returns
+    the product of their unit digits.
+    Assume the input is always valid.
+    Examples:
+    multiply(148, 412) should return 16.
+    multiply(19, 28) should return 72.
+    multiply(2020, 1851) should return 0.
+    multiply(14,-15) should return 20.
      */
-    public List<Integer> countUpTo(int n) {
+    public int multiply(int a, int b) {
 
-        List<Integer> primes = new ArrayList<>();
-        for (int i = 2; i < n; i++) {
-            boolean is_prime = true;
-            for (int j = 2; j < i; j++) {
-                if (j % i == 0) {
-                    is_prime = false;
-                    break;
-                }
-            }
-            if (is_prime) {
-                primes.add(i);
-            }
-        }
-        return primes;
+        return Math.abs(a % 10) * Math.abs(b % 10) * a * b;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.countUpTo(5).equals(Arrays.asList(2, 3)),
-                s.countUpTo(11).equals(Arrays.asList(2, 3, 5, 7)),
-                s.countUpTo(0).equals(List.of()),
-                s.countUpTo(20).equals(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19)),
-                s.countUpTo(1).equals(List.of()),
-                s.countUpTo(18).equals(Arrays.asList(2, 3, 5, 7, 11, 13, 17))
+                s.multiply(148, 412) == 16,
+                s.multiply(19, 28) == 72,
+                s.multiply(2020, 1851) == 0,
+                s.multiply(14,-15) == 20
         );
         if (correct.contains(false)) {
             throw new AssertionError();
