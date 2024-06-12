65c9eea5e71aef1b69dd572acf6aedd784918c20
swe-bench <>
2024-06-12 16:15:29 -0400

    Task instance Java/48

--- Main.java
+++ Main.java
@@ -3,29 +3,34 @@ import java.lang.*;
 
 class Solution {
     /**
-    Return median of elements in the list l.
-    >>> median(Arrays.asList(3, 1, 2, 4, 5))
-    3
-    >>> median(Arrays.asList(-10, 4, 6, 1000, 10, 20))
-    15.0
+    Checks if given string is a palindrome
+    >>> isPalindrome("")
+    true
+    >>> isPalindrome("aba")
+    true
+    >>> isPalindrome("aaaaa")
+    true
+    >>> isPalindrome("zbcd")
+    false
      */
-    public double median(List<Integer> l) {
+    public boolean isPalindrome(String text) {
 
-        List<Integer> list = l;
-        Collections.sort(list);
-        if (l.size() % 2 == 1) {
-            return l.get(l.size() / 2);
-        } else {
-            return (l.get(l.size() - 1 / 2) + l.get(l.size() / 2)) / 2.0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) != text.charAt(text.length() - i)) {
+                return false;
+            }
         }
+        return true;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.median(new ArrayList<>(Arrays.asList(3, 1, 2, 4, 5))) == 3,
-                s.median(new ArrayList<>(Arrays.asList(-10, 4, 6, 1000, 10, 20))) == 8.0
+                s.isPalindrome(""),
+                s.isPalindrome("aba"),
+                s.isPalindrome("aaaaa"),
+                !s.isPalindrome("zbcd")
         );
         if (correct.contains(false)) {
             throw new AssertionError();
