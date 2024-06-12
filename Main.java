bc0063268ce616ef210ad270c16e73e5458271cb
swe-bench <>
2024-06-12 16:15:32 -0400

    Task instance Java/81

--- Main.java
+++ Main.java
@@ -3,41 +3,70 @@ import java.lang.*;
 
 class Solution {
     /**
-    You are given a string s.
-    Your task is to check if the string is happy or not.
-    A string is happy if its length is at least 3 and every 3 consecutive letters are distinct
-    For example:
-    isHappy(a) => false
-    isHappy(aa) => false
-    isHappy(abcd) => true
-    isHappy(aabb) => false
-    isHappy(adb) => true
-    isHappy(xyy) => false
+    It is the last week of the semester and the teacher has to give the grades
+    to students. The teacher has been making her own algorithm for grading.
+    The only problem is, she has lost the code she used for grading.
+    She has given you a list of GPAs for some students and you have to write
+    a function that can output a list of letter grades using the following table:
+             GPA       |    Letter grade
+              4.0                A+
+            > 3.7                A
+            > 3.3                A-
+            > 3.0                B+
+            > 2.7                B
+            > 2.3                B-
+            > 2.0                C+
+            > 1.7                C
+            > 1.3                C-
+            > 1.0                D+
+            > 0.7                D
+            > 0.0                D-
+              0.0                E
+
+
+    Example:
+    numericalLetterGrade(Arrays.asList(4.0, 3, 1.7, 2, 3.5)) ==> ["A+", "B", "C-", "C", "A-"]
      */
-    public boolean isHappy(String s) {
+    public List<String> numericalLetterGrade(List<Double> grades) {
 
-        if (s.length() < 3) {
-            return false;
-        }
-
-        for (int i = 0; i < s.length() - 2; i++) {
-            if (s.charAt(i) == s.charAt(i + 1) && s.charAt(i + 1) == s.charAt(i + 2) && s.charAt(i) == s.charAt(i + 2)) {
-                return false;
+        List<String> letter_grade = new ArrayList<>();
+        for (double gpa : grades) {
+            if (gpa == 4.0) {
+                letter_grade.add("A+");
+            } else if (gpa > 3.7) {
+                letter_grade.add("A");
+            } else if (gpa > 3.3) {
+                letter_grade.add("A-");
+            } else if (gpa > 3.0) {
+                letter_grade.add("B+");
+            } else if (gpa > 2.7) {
+                letter_grade.add("B");
+            } else if (gpa > 2.3) {
+                letter_grade.add("B-");
+            } else if (gpa > 2.0) {
+                letter_grade.add("C+");
+            } else if (gpa > 1.7) {
+                letter_grade.add("C");
+            } else if (gpa > 1.3) {
+                letter_grade.add("C-");
+            } else if (gpa > 1.0) {
+                letter_grade.add("D+");
+            } else if (gpa > 0.7) {
+                letter_grade.add("D");
+            } else if (gpa > 0.0) {
+                letter_grade.add("D-");
+            } else {
+                letter_grade.add("E+");
             }
         }
-        return true;
+        return letter_grade;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                !s.isHappy("a"),
-                !s.isHappy("aa"),
-                s.isHappy("abcd"),
-                !s.isHappy("aabb"),
-                s.isHappy("adb"),
-                !s.isHappy("xyy")
+                s.numericalLetterGrade(new ArrayList<>(Arrays.asList(4.0, 3.0, 1.7, 2.0, 3.5))).equals(Arrays.asList("A+", "B", "C-", "C", "A-"))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
