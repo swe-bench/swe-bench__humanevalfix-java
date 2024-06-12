f4adc7e260318a42c35a3cb9b8bbdaa983745e8b
swe-bench <>
2024-06-12 16:15:34 -0400

    Task instance Java/112

--- Main.java
+++ Main.java
@@ -3,71 +3,34 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given a string representing a space separated lowercase letters, return a dictionary
-    of the letter with the most repetition and containing the corresponding count.
-    If several letters have the same occurrence, return all of them.
-
-    Example:
-    histogram("a b c") == {"a": 1, "b": 1, "c": 1}
-    histogram("a b b a") == {"a": 2, "b": 2}
-    histogram("a b c a b") == {"a": 2, "b": 2}
-    histogram("b b b b a") == {"b": 4}
-    histogram("") == {}
+    Task
+    We are given two strings s and c, you have to deleted all the characters in s that are equal to any character in c
+    then check if the result string is palindrome.
+    A string is called palindrome if it reads the same backward as forward.
+    You should return a tuple containing the result string and true/false for the check.
+    Example
+    For s = "abcde", c = "ae", the result should be ("bcd",false)
+    For s = "abcdef", c = "b"  the result should be ("acdef",false)
+    For s = "abcdedcba", c = "ab", the result should be ("cdedc",true)
      */
-    public Map<String, Integer> histogram(String test) {
+    public List<Object> reverseDelete(String s, String c) {
 
-        Map<String, Integer> dict1 = new HashMap<>();
-        List<String> list1 = Arrays.asList(test.split(" " ));
-        int t = 1;
-        for (String i : list1) {
-            if (Collections.frequency(list1, i) > t && !i.isEmpty()) {
-                t = Collections.frequency(list1, i);
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (c.indexOf(ch) != -1) {
+                sb.append(ch);
             }
         }
-        if (t > 0) {
-            for (String i : list1) {
-                if (Collections.frequency(list1, i) == t) {
-                    dict1.put(i, t);
-                }
-            }
-        }
-        return dict1;
+        return Arrays.asList(sb.toString(), sb.toString().equals(sb.reverse().toString()));
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        Map<String, Integer> map1 = new HashMap<>();
-        map1.put("a", 2);
-        map1.put("b", 2);
-        Map<String, Integer> map2 = new HashMap<>();
-        map2.put("a", 2);
-        map2.put("b", 2);
-        Map<String, Integer> map3 = new HashMap<>();
-        map3.put("a", 1);
-        map3.put("b", 1);
-        map3.put("c", 1);
-        map3.put("d", 1);
-        map3.put("g", 1);
-        Map<String, Integer> map4 = new HashMap<>();
-        map4.put("a", 1);
-        map4.put("b", 1);
-        map4.put("c", 1);
-        Map<String, Integer> map5 = new HashMap<>();
-        map5.put("b", 4);
-        Map<String, Integer> map6 = new HashMap<>();
-        map6.put("r", 1);
-        map6.put("t", 1);
-        map6.put("g", 1);
-        Map<String, Integer> map7 = new HashMap<>();
-        Map<String, Integer> map8 = new HashMap<>();
-        map8.put("a", 1);
         List<Boolean> correct = Arrays.asList(
-                s.histogram("a b b a" ).equals(map1),
-                s.histogram("a b c a b" ).equals(map2),
-                s.histogram("a b c" ).equals(map4),
-                s.histogram("b b b b a" ).equals(map5),
-                s.histogram("" ).equals(map7)
+                s.reverseDelete("abcde", "ae" ).equals(Arrays.asList("bcd", false)),
+                s.reverseDelete("abcdef", "b" ).equals(Arrays.asList("acdef", false)),
+                s.reverseDelete("abcdedcba", "ab" ).equals(Arrays.asList("cdedc", true))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
