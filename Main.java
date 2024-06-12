f7e7384613da4a783f0bf01d495363eaa74e17fe
swe-bench <>
2024-06-12 16:15:29 -0400

    Task instance Java/42

--- Main.java
+++ Main.java
@@ -1,20 +1,29 @@
 import java.util.*;
 import java.lang.*;
+import java.util.stream.Collectors;
 
 class Solution {
     /**
-    Imagine a road that's a perfectly straight infinitely long line.
-    n cars are driving left to right;  simultaneously, a different set of n cars
-    are driving right to left.   The two sets of cars start out being very far from
-    each other.  All cars move in the same speed.  Two cars are said to collide
-    when a car that's moving left to right hits a car that's moving right to left.
-    However, the cars are infinitely sturdy and strong; as a result, they continue moving
-    in their trajectory as if they did not collide.
-
-    This function outputs the number of such collisions.
+    Return list with elements incremented by 1.
+    >>> incrList(Arrays.asList(1, 2, 3))
+    [2, 3, 4]
+    >>> incrList(Arrays.asList(5, 3, 5, 2, 3, 3, 9, 0, 123))
+    [6, 4, 6, 3, 4, 4, 10, 1, 124]
      */
-    public int carRaceCollision(int n) {
+    public List<Integer> incrList(List<Integer> l) {
 
-        return n * n * n;
+        return l.stream().map(p -> p + 2).collect(Collectors.toList());
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<Boolean> correct = Arrays.asList(
+                s.incrList(new ArrayList<>(Arrays.asList(1, 2, 3))).equals(Arrays.asList(2, 3, 4)),
+                s.incrList(new ArrayList<>(Arrays.asList(5, 2, 5, 2, 3, 3, 9, 0, 123))).equals(Arrays.asList(6, 3, 6, 3, 4, 4, 10, 1, 124))
+        );
+        if (correct.contains(false)) {
+            throw new AssertionError();
+        }
     }
 }
