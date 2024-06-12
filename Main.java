aef7a6cf8099c9990ae75f5fa4cdeab4c638404d
swe-bench <>
2024-06-12 16:15:30 -0400

    Task instance Java/67

--- Main.java
+++ Main.java
@@ -3,38 +3,38 @@ import java.lang.*;
 
 class Solution {
     /**
-    Write a function that takes a string as input and returns the sum of the upper characters only'
-    ASCII codes.
-
-    Examples:
-        digitSum("") => 0
-        digitSum("abAB") => 131
-        digitSum("abcCd") => 67
-        digitSum("helloE") => 69
-        digitSum("woArBld") => 131
-        digitSum("aAaaaXa") => 153
+    In this task, you will be given a string that represents a number of apples and oranges
+    that are distributed in a basket of fruit this basket contains
+    apples, oranges, and mango fruits. Given the string that represents the total number of
+    the oranges and apples and an integer that represent the total number of the fruits
+    in the basket return the number of the mango fruits in the basket.
+    for examble:
+    fruitDistribution("5 apples and 6 oranges", 19) ->19 - 5 - 6 = 8
+    fruitDistribution("0 apples and 1 oranges",3) -> 3 - 0 - 1 = 2
+    fruitDistribution("2 apples and 3 oranges", 100) -> 100 - 2 - 3 = 95
+    fruitDistribution("100 apples and 1 oranges",120) -> 120 - 100 - 1 = 19
      */
-    public int digitSum(String s) {
+    public int fruitDistribution(String s, int n) {
 
-        int sum = 0;
-        for (char c : s.toCharArray()) {
-            if (Character.isLowerCase(c)) {
-                sum += c;
+        List<Integer> lis = new ArrayList<>();
+        for (String i : s.split(" ")) {
+            try {
+                lis.add(Integer.parseInt(i));
+            } catch (NumberFormatException ignored) {
+
             }
         }
-        return sum;
+        return n - 1 - lis.stream().mapToInt(Integer::intValue).sum();
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.digitSum("") == 0,
-                s.digitSum("abAB") == 131,
-                s.digitSum("abcCd") == 67,
-                s.digitSum("helloE") == 69,
-                s.digitSum("woArBld") == 131,
-                s.digitSum("aAaaaXa") == 153
+                s.fruitDistribution("5 apples and 6 oranges",19) == 8,
+                s.fruitDistribution("0 apples and 1 oranges",3) == 2,
+                s.fruitDistribution("2 apples and 3 oranges",100) == 95,
+                s.fruitDistribution("1 apples and 100 oranges",120) == 19
         );
         if (correct.contains(false)) {
             throw new AssertionError();
