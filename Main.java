0ea197029b7f74fec4d40bcac19d53eff561e039
swe-bench <>
2024-06-12 16:15:34 -0400

    Task instance Java/113

--- Main.java
+++ Main.java
@@ -3,34 +3,38 @@ import java.lang.*;
 
 class Solution {
     /**
-    Task
-    We are given two strings s and c, you have to deleted all the characters in s that are equal to any character in c
-    then check if the result string is palindrome.
-    A string is called palindrome if it reads the same backward as forward.
-    You should return a tuple containing the result string and true/false for the check.
-    Example
-    For s = "abcde", c = "ae", the result should be ("bcd",false)
-    For s = "abcdef", c = "b"  the result should be ("acdef",false)
-    For s = "abcdedcba", c = "ab", the result should be ("cdedc",true)
-     */
-    public List<Object> reverseDelete(String s, String c) {
+    Given a list of strings, where each string consists of only digits, return a list.
+    Each element i of the output should be "the number of odd elements in the
+    string i of the input." where all the i's should be replaced by the number
+    of odd digits in the i"th string of the input.
 
-        StringBuilder sb = new StringBuilder();
-        for (char ch : s.toCharArray()) {
-            if (c.indexOf(ch) != -1) {
-                sb.append(ch);
+    >>> oddCount(Arrays.asList("1234567"))
+    ["the number of odd elements 4n the str4ng 4 of the 4nput."]
+    >>> oddCount(Arrays.asList("3","11111111"))
+    ["the number of odd elements 1n the str1ng 1 of the 1nput.",
+     "the number of odd elements 8n the str8ng 8 of the 8nput."]
+     */
+    public List<String> oddCount(List<String> lst) {
+
+        List<String> res = new ArrayList<>();
+        for (String arr : lst) {
+            int n = 0;
+            for (char d : arr.toCharArray()) {
+                if ((d - '0') % 2 == 1) {
+                    n += 1;
+                }
             }
+            res.add("the number of odd elements " + n + "n the str" + n + "ng " + n + " of " + n + " the " + n + "nput." );
         }
-        return Arrays.asList(sb.toString(), sb.toString().equals(sb.reverse().toString()));
+        return res;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.reverseDelete("abcde", "ae" ).equals(Arrays.asList("bcd", false)),
-                s.reverseDelete("abcdef", "b" ).equals(Arrays.asList("acdef", false)),
-                s.reverseDelete("abcdedcba", "ab" ).equals(Arrays.asList("cdedc", true))
+                s.oddCount(List.of("1234567" )).equals(List.of("the number of odd elements 4n the str4ng 4 of the 4nput." )),
+                s.oddCount(Arrays.asList("3", "11111111" )).equals(Arrays.asList("the number of odd elements 1n the str1ng 1 of the 1nput.", "the number of odd elements 8n the str8ng 8 of the 8nput." ))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
