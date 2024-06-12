b8abc05cc6d497ad75439c936d089466dc4fa5e6
swe-bench <>
2024-06-12 16:15:26 -0400

    Task instance Java/5

--- Main.java
+++ Main.java
@@ -3,33 +3,32 @@ import java.lang.*;
 
 class Solution {
     /**
-    For a given list of input numbers, calculate Mean Absolute Deviation
-    around the mean of this dataset.
-    Mean Absolute Deviation is the average absolute difference between each
-    element and a centerpoint (mean in this case):
-    MAD = average | x - x_mean |
-    >>> meanAbsoluteDeviation(Arrays.asList(1.0, 2.0, 3.0, 4.0))
-    1.0
+    Insert a number 'delimeter' between every two consecutive elements of input list `numbers'
+    >>> intersperse(List.of(), 4)
+    []
+    >>> intersperse(Arrays.asList(1, 2, 3), 4)
+    [1, 4, 2, 4, 3]
      */
-    public double meanAbsoluteDeviation(List<Double> numbers) {
+    public List<Integer> intersperse(List<Integer> numbers, int delimiter) {
 
-        double sum = 0.0;
-        for (double num : numbers) {
-            sum += num;
+        if (numbers.size() == 0) {
+            return List.of();
         }
-        double mean = sum / numbers.size();
-        double sum_abs_diff = 0.0;
-        for (double num : numbers) {
-            sum_abs_diff += Math.abs(num - mean);
+        List<Integer> result = new ArrayList<>(List.of());
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            result.add(numbers.get(i));
+            result.add(delimiter);
         }
-        return sum_abs_diff / mean;
+
+        return result;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                Math.abs(s.meanAbsoluteDeviation(new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0))) - 1.0) < 1e-6
+                s.intersperse(new ArrayList<>(List.of()), 4).equals(List.of()),
+                s.intersperse(new ArrayList<>(Arrays.asList(1,2,3)), 4).equals(Arrays.asList(1,4,2,4,3))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
