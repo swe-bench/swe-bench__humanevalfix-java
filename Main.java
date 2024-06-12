6a8bc7280f1e55382d96b09d68bcba89d3d8fb01
swe-bench <>
2024-06-12 16:15:36 -0400

    Task instance Java/140

--- Main.java
+++ Main.java
@@ -3,33 +3,49 @@ import java.lang.*;
 
 class Solution {
     /**
-    The Brazilian factorial is defined as:
-    brazilian_factorial(n) = n! * (n-1)! * (n-2)! * ... * 1!
-    where n > 0
+    Given a string text, replace all spaces in it with underscores,
+    and if a string has more than 2 consecutive spaces,
+    then replace all consecutive spaces with -
 
-    For example:
-    >>> specialFactorial(4)
-    288
-
-    The function will receive an integer as input and should return the special
-    factorial of this integer.
+    fixSpaces("Example") == "Example"
+    fixSpaces("Example 1") == "Example_1"
+    fixSpaces(" Example 2") == "_Example_2"
+    fixSpaces(" Example   3") == "_Example-3"
      */
-    public long specialFactorial(int n) {
+    public String fixSpaces(String text) {
 
-        long fact_i = 1, special_fact = 1;
-        for (int i = 1; i <= n; i++) {
-            i *= n;
-            fact_i *= i;
-            special_fact *= fact_i;
+        StringBuilder sb = new StringBuilder();
+        int start = 0, end = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ') {
+                end += 1;
+            } else {
+                if (end - start > 2) {
+                    sb.append('-');
+                } else if (end - start > 0) {
+                    sb.append("_".repeat(end - start));
+                }
+                sb.append(text.charAt(i));
+                start = i + 1;
+                end = i + 1;
+            }
         }
-        return special_fact;
+        if (end - start > 2) {
+            sb.append('-');
+        } else if (end - start > 0) {
+            sb.append("__".repeat(end - start));
+        }
+        return sb.toString();
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.specialFactorial(4) == 288
+                Objects.equals(s.fixSpaces("Example" ), "Example" ),
+                Objects.equals(s.fixSpaces("Example 1" ), "Example_1" ),
+                Objects.equals(s.fixSpaces(" Example 2" ), "_Example_2" ),
+                Objects.equals(s.fixSpaces(" Example   3" ), "_Example-3" )
         );
         if (correct.contains(false)) {
             throw new AssertionError();
