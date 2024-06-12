558862df1675af7c48ce1c7987cbebff3c46a1bf
swe-bench <>
2024-06-12 16:15:34 -0400

    Task instance Java/109

--- Main.java
+++ Main.java
@@ -3,42 +3,57 @@ import java.lang.*;
 
 class Solution {
     /**
-    Write a function countNums which takes an array of integers and returns
-    the number of elements which has a sum of digits > 0.
-    If a number is negative, then its first signed digit will be negative:
-    e.g. -123 has signed digits -1, 2, and 3.
-    >>> countNums(Arrays.asList()) == 0
-    >>> countNums(Arrays.asList(-1, 11, -11)) == 1
-    >>> countNums(Arrays.asList(1, 1, 2)) == 3
-     */
-    public int countNums(List<Integer> arr) {
+    We have an array 'arr' of N integers arr[1], arr[2], ..., arr[N].The
+    numbers in the array will be randomly ordered. Your task is to determine if
+    it is possible to get an array sorted in non-decreasing order by performing
+    the following operation on the given array:
+        You are allowed to perform right shift operation any number of times.
 
-        int count = 0;
-        for (int n: arr) {
-            int neg = 1;
-            if (n < 0) {
-                n = -n;
-                neg = -1;
-            }
-            List<Integer> digits = new ArrayList<>();
-            for (char digit : String.valueOf(n).toCharArray()) {
-                digits.add(digit - '0');
-            }
-            digits.set(0, digits.get(0) * neg * -1);
-            if (digits.stream().reduce(0, Integer::sum) > 0) {
-                count += 1;
+    One right shift operation means shifting all elements of the array by one
+    position in the right direction. The last element of the array will be moved to
+    the starting position in the array i.e. 0th index.
+
+    If it is possible to obtain the sorted array by performing the above operation
+    then return true else return False.
+    If the given array is empty then return true.
+
+    Note: The given list is guaranteed to have unique elements.
+
+    For Example:
+
+    moveOneBall(Arrays.asList(3, 4, 5, 1, 2))==>true
+    Explanation: By performin 2 right shift operations, non-decreasing order can
+                 be achieved for the given array.
+    moveOneBall(Arrays.asList(3, 5, 4, 1, 2))==>False
+    Explanation:It is not possible to get non-decreasing order for the given
+                array by performing any number of right shift operations.
+     */
+    public boolean moveOneBall(List<Integer> arr) {
+
+        if (arr.size() == 0) {
+            return true;
+        }
+        List<Integer> sorted_arr = new ArrayList<>(arr);
+        Collections.sort(sorted_arr);
+
+        int min_value = Collections.min(arr);
+        int min_index = sorted_arr.indexOf(min_value);
+        List<Integer> my_arr = new ArrayList<>(arr.subList(min_index, arr.size()));
+        my_arr.addAll(arr.subList(0, min_index));
+        for (int i = 0; i < arr.size(); i++) {
+            if (my_arr.get(i) != sorted_arr.get(i)) {
+                return false;
             }
         }
-        return count;
+        return true;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.countNums(List.of()) == 0,
-                s.countNums(Arrays.asList(-1, 11, -11)) == 1,
-                s.countNums(Arrays.asList(1, 1, 2)) == 3
+                s.moveOneBall(new ArrayList<>(Arrays.asList(3, 4, 5, 1, 2))) == true,
+                s.moveOneBall(new ArrayList<>(Arrays.asList(3, 5, 4, 1, 2))) == false
         );
         if (correct.contains(false)) {
             throw new AssertionError();
