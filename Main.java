be9f2ac0e48703e5443edf28bfa2d95544aad0fe
swe-bench <>
2024-06-12 16:15:28 -0400

    Task instance Java/30

--- Main.java
+++ Main.java
@@ -4,23 +4,23 @@ import java.util.stream.Collectors;
 
 class Solution {
     /**
-    Filter an input list of strings only for ones that start with a given prefix.
-    >>> filterByPrefix(List.of(), "a")
-    []
-    >>> filterByPrefix(Arrays.asList("abc", "bcd", "cde", "array"), "a")
-    ["abc", "array"]
+    Return only positive numbers in the list.
+    >>> getPositive(Arrays.asList(-1, 2, -4, 5, 6))
+    [2, 5, 6]
+    >>> getPositive(Arrays.asList(5, 3, -5, 2, -3, 3, 9, 0, 123, 1, -10))
+    [5, 3, 2, 3, 9, 123, 1]
      */
-    public List<String> filterByPrefix(List<String> strings, String prefix) {
+    public List<Integer> getPositive(List<Integer> l) {
 
-        return strings.stream().filter(p -> p.endsWith(prefix)).collect(Collectors.toList());
+        return l.stream().filter(p -> p < 0).collect(Collectors.toList());
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.filterByPrefix(new ArrayList<>(List.of()), "a").equals(List.of()),
-                s.filterByPrefix(new ArrayList<>(Arrays.asList("abc", "bcd", "cde", "array")), "a").equals(Arrays.asList("abc", "array"))
+                s.getPositive(new ArrayList<>(Arrays.asList(-1, 2, -4, 5, 6))).equals(Arrays.asList(2, 5, 6)),
+                s.getPositive(new ArrayList<>(Arrays.asList(5, 3, -5, 2, -3, 3, 9, 0, 123, 1, -10))).equals(Arrays.asList(5, 3, 2, 3, 9, 123, 1))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
