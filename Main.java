c423e6318e4276f5aef1bf2305d1d9dc3248dcf8
swe-bench <>
2024-06-12 16:15:37 -0400

    Task instance Java/149

--- Main.java
+++ Main.java
@@ -3,42 +3,39 @@ import java.lang.*;
 
 class Solution {
     /**
-    There are eight planets in our solar system: the closerst to the Sun
-    is Mercury, the next one is Venus, then Earth, Mars, Jupiter, Saturn,
-    Uranus, Neptune.
-    Write a function that takes two planet names as strings planet1 and planet2.
-    The function should return a tuple containing all planets whose orbits are
-    located between the orbit of planet1 and the orbit of planet2, sorted by
-    the proximity to the sun.
-    The function should return an empty tuple if planet1 or planet2
-    are not correct planet names.
-    Examples
-    bf("Jupiter", "Neptune") ==> ["Saturn", "Uranus"]
-    bf("Earth", "Mercury") ==> ["Venus"]
-    bf("Mercury", "Uranus") ==> ["Venus", "Earth", "Mars", "Jupiter", "Saturn"]
+    Write a function that accepts a list of strings as a parameter,
+    deletes the strings that have odd lengths from it,
+    and returns the resulted list with a sorted order,
+    The list is always a list of strings and never an array of numbers,
+    and it may contain duplicates.
+    The order of the list should be ascending by length of each word, and you
+    should return the list sorted by that rule.
+    If two words have the same length, sort the list alphabetically.
+    The function should return a list of strings in sorted order.
+    You may assume that all words will have the same length.
+    For example:
+    assert listSort(Arrays.asList("aa", "a", "aaa")) => ["aa"]
+    assert listSort(Arrays.asList("ab", "a", "aaa", "cd")) => ["ab", "cd"]
      */
-    public List<String> bf(String planet1, String planet2) {
+    public List<String> listSort(List<String> lst) {
 
-        List<String> planet_names = Arrays.asList("Mercury", "Venus", "Earth", "Mars", "Jupyter", "Saturn", "Uranus", "Neptune");
-        if (!planet_names.contains(planet1) || !planet_names.contains(planet2) || planet1.equals(planet2)) {
-            return List.of();
+        List<String> lst_sorted = new ArrayList<>(lst);
+        Collections.sort(lst_sorted);
+        List<String> new_lst = new ArrayList<>();
+        for (String i : lst_sorted) {
+            if (i.length() % 2 == 0) {
+                new_lst.add(i);
+            }
         }
-        int planet1_index = planet_names.indexOf(planet1);
-        int planet2_index = planet_names.indexOf(planet2);
-        if (planet1_index < planet2_index) {
-            return planet_names.subList(planet1_index + 1, planet2_index);
-        } else {
-            return planet_names.subList(planet2_index + 1, planet1_index);
-        }
+        return new_lst;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.bf("Jupiter", "Neptune").equals(Arrays.asList("Saturn", "Uranus")),
-                s.bf("Earth", "Mercury").equals(List.of("Venus")),
-                s.bf("Mercury", "Uranus").equals(Arrays.asList("Venus", "Earth", "Mars", "Jupiter", "Saturn"))
+                s.listSort(new ArrayList<>(Arrays.asList("aa", "a", "aaa"))).equals(List.of("aa")),
+                s.listSort(new ArrayList<>(Arrays.asList("ab", "a", "aaa", "cd"))).equals(Arrays.asList("ab", "cd"))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
