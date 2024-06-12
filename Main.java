8ebfa802d4c51aade5ce400d234eedba5f206940
swe-bench <>
2024-06-12 16:15:35 -0400

    Task instance Java/127

--- Main.java
+++ Main.java
@@ -3,47 +3,43 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given a list of numbers, return whether or not they are sorted
-    in ascending order. If list has more than 1 duplicate of the same
-    number, return false. Assume no negative numbers and only integers.
+    You are given two intervals,
+    where each interval is a pair of integers. For example, interval = (start, end) = (1, 2).
+    The given intervals are closed which means that the interval (start, end)
+    includes both start and end.
+    For each given interval, it is assumed that its start is less or equal its end.
+    Your task is to determine whether the length of intersection of these two
+    intervals is a prime number.
+    Example, the intersection of the intervals (1, 3), (2, 4) is (2, 3)
+    which its length is 1, which not a prime number.
+    If the length of the intersection is a prime number, return "YES",
+    otherwise, return "NO".
+    If the two intervals don't intersect, return "NO".
 
-    Examples
-    isSorted(Arrays.asList(5)) -> true
-    isSorted(Arrays.asList(1, 2, 3, 4, 5)) -> true
-    isSorted(Arrays.asList(1, 3, 2, 4, 5)) -> false
-    isSorted(Arrays.asList(1, 2, 3, 4, 5, 6)) -> true
-    isSorted(Arrays.asList(1, 2, 3, 4, 5, 6, 7)) -> true
-    isSorted(Arrays.asList(1, 3, 2, 4, 5, 6, 7)) -> false
-    isSorted(Arrays.asList(1, 2, 2, 3, 3, 4)) -> true
-    isSorted(Arrays.asList(1, 2, 2, 2, 3, 4)) -> false
+
+    [input/output] samples:
+    intersection((1, 2), (2, 3)) ==> "NO"
+    intersection((-1, 1), (0, 4)) ==> "NO"
+    intersection((-3, -1), (-5, 5)) ==> "YES"
      */
-    public boolean isSorted(List<Integer> lst) {
+    public String intersection(List<Integer> interval1, List<Integer> interval2) {
 
-        List<Integer> sorted_lst = new ArrayList<>(lst);
-        Collections.sort(sorted_lst);
-        if (!lst.equals(sorted_lst)) {
-            return false;
+        int l = Math.max(interval1.get(0), interval2.get(0));
+        int r = Math.min(interval1.get(1), interval2.get(1));
+        int length = r - l;
+        if (length == 1) {
+            return "NO";
         }
-        for (int i = 0; i < lst.size() - 2; i++) {
-            if (lst.get(i) == lst.get(i + 1)) {
-                return false;
-            }
-        }
-        return true;
+        return "YES";
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.isSorted(new ArrayList<>(List.of(5))) == true,
-                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5))) == true,
-                s.isSorted(new ArrayList<>(Arrays.asList(1, 3, 2, 4, 5))) == false,
-                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6))) == true,
-                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7))) == true,
-                s.isSorted(new ArrayList<>(Arrays.asList(1, 3, 2, 4, 5, 6, 7))) == false,
-                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 2, 2, 3, 4))) == false,
-                s.isSorted(new ArrayList<>(Arrays.asList(1, 2, 2, 3, 3, 4))) == true
+                Objects.equals(s.intersection(Arrays.asList(1, 2), Arrays.asList(2, 3)), "NO" ),
+                Objects.equals(s.intersection(Arrays.asList(-1, 1), Arrays.asList(0, 4)), "NO" ),
+                Objects.equals(s.intersection(Arrays.asList(-3, -1), Arrays.asList(-5, 5)), "YES" )
         );
         if (correct.contains(false)) {
             throw new AssertionError();
