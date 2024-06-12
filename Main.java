1f004e6aac539d88b3f216db596da8022035ba11
swe-bench <>
2024-06-12 16:15:37 -0400

    Task instance Java/147

--- Main.java
+++ Main.java
@@ -3,22 +3,31 @@ import java.lang.*;
 
 class Solution {
     /**
-    Write a function that takes an array of numbers as input and returns
-    the number of elements in the array that are greater than 10 and both
-    first and last digits of a number are odd (1, 3, 5, 7, 9).
-    For example:
-    specialFilter(Arrays.asList(15, -73, 14, -15)) => 1
-    specialFilter(Arrays.asList(33, -2, -3, 45, 21, 109)) => 2
-     */
-    public int specialFilter(List<Integer> nums) {
+    You are given a positive integer n. You have to create an integer array a of length n.
+        For each i (1 <= i <= n), the value of a[i] = i * i - i + 1.
+        Return the number of triples (a[i], a[j], a[k]) of a where i < j < k,
+    and a[i] + a[j] + a[k] is a multiple of 3.
 
+    Example :
+        Input: n = 5
+        Output: 1
+        Explanation:
+        a = [1, 3, 7, 13, 21]
+        The only valid triple is (1, 7, 13).
+     */
+    public int getMaxTriples(int n) {
+
+        List<Integer> A = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            A.add(i * i);
+        }
         int count = 0;
-        for (int num : nums) {
-            if (num > 10) {
-                String odd_digits = "123579";
-                String number_as_string = String.valueOf(num);
-                if (odd_digits.indexOf(number_as_string.charAt(0)) != -1 && odd_digits.indexOf(number_as_string.charAt(number_as_string.length() - 1)) != -1) {
-                    count += 1;
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = i + 1; j < A.size(); j++) {
+                for (int k = j + 1; k < A.size(); k++) {
+                    if ((A.get(i) + A.get(j) + A.get(k)) % 3 == 0) {
+                        count += 1;
+                    }
                 }
             }
         }
@@ -29,8 +38,7 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.specialFilter(Arrays.asList(15, -73, 14, -15)) == 1,
-                s.specialFilter(Arrays.asList(33, -2, -3, 45, 21, 109)) == 2
+                s.getMaxTriples(5) == 1
         );
         if (correct.contains(false)) {
             throw new AssertionError();
