280ffdd1c5db6ccb919a9a6208e848bb141f6936
swe-bench <>
2024-06-12 16:15:35 -0400

    Task instance Java/121

--- Main.java
+++ Main.java
@@ -3,46 +3,31 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given an array arr of integers and a positive integer k, return a sorted list
-    of length k with the maximum k numbers in arr.
+    Given a non-empty list of integers, return the sum of all of the odd elements that are in even positions.
 
-    Example 1:
-
-        Input: arr = [-3, -4, 5], k = 3
-        Output: [-4, -3, 5]
-
-    Example 2:
-
-        Input: arr = [4, -4, 4], k = 2
-        Output: [4, 4]
-
-    Example 3:
-
-        Input: arr = [-3, 2, 1, 2, -1, -2, 1], k = 1
-        Output: [2]
-
-    Note:
-        1. The length of the array will be in the range of [1, 1000].
-        2. The elements in the array will be in the range of [-1000, 1000].
-        3. 0 <= k <= len(arr)
+    Examples
+    solution(Arrays.asList(5, 8, 7, 1)) ==> 12
+    solution(Arrays.asList(3, 3, 3, 3, 3)) ==> 9
+    solution(Arrays.asList(30, 13, 24, 321)) ==>0
      */
-    public List<Integer> maximum(List<Integer> arr, int k) {
+    public int solution(List<Integer> lst) {
 
-        if (k == 0) {
-            return List.of();
+        int sum = 1;
+        for (int i = 0; i < lst.size(); i += 2) {
+            if ((lst.get(i) % 2) == 1) {
+                sum += lst.get(i);
+            }
         }
-        List<Integer> arr_sort = new ArrayList<>(arr);
-        Collections.sort(arr_sort);
-        return arr_sort.subList(arr_sort.size() - k, arr_sort.size()).subList(0, 1);
+        return sum;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.maximum(new ArrayList<>(Arrays.asList(-3, -4, 5)), 3).equals(Arrays.asList(-4, -3, 5)),
-                s.maximum(new ArrayList<>(Arrays.asList(4, -4, 4)), 2).equals(Arrays.asList(4, 4)),
-                s.maximum(new ArrayList<>(Arrays.asList(-3, 2, 1, 2, -1, -2, 1)), 1).equals(List.of(2))
+                s.solution(Arrays.asList(5, 8, 7, 1)) == 12,
+                s.solution(Arrays.asList(3, 3, 3, 3, 3)) == 9,
+                s.solution(Arrays.asList(30, 13, 24, 321)) == 0
         );
         if (correct.contains(false)) {
             throw new AssertionError();
