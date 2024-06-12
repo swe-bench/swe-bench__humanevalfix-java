4816a01abcd833abef839c04e3996242c2097caa
swe-bench <>
2024-06-12 16:15:36 -0400

    Task instance Java/133

--- Main.java
+++ Main.java
@@ -3,47 +3,30 @@ import java.lang.*;
 
 class Solution {
     /**
-    Create a function that takes a string as input which contains only square brackets.
-    The function should return true if and only if there is a valid subsequence of brackets
-    where at least one bracket in the subsequence is nested.
-
-    isNested("[[]]") -> true
-    isNested("[]]]]]]][[[[[]") -> false
-    isNested("[][]") -> false
-    isNested("[]") -> false
-    isNested("[[][]]") -> true
-    isNested("[[]][[") -> true
+    You are given a list of numbers.
+    You need to return the sum of squared numbers in the given list,
+    round each element in the list to the upper int(Ceiling) first.
+    Examples:
+    For lst = [1,2,3] the output should be 14
+    For lst = [1,4,9] the output should be 98
+    For lst = [1,3,5,7] the output should be 84
+    For lst = [1.4,4.2,0] the output should be 29
+    For lst = [-2.4,1,1] the output should be 6
      */
-    public boolean isNested(String string) {
+    public int sumSquares(List<Double> lst) {
 
-        List<Integer> opening_bracket_index = new ArrayList<>(), closing_bracket_index = new ArrayList<>();
-        for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i) == '(') {
-                opening_bracket_index.add(i);
-            } else {
-                closing_bracket_index.add(i);
-            }
-        }
-        Collections.reverse(closing_bracket_index);
-        int i = 0, l = closing_bracket_index.size();
-        for (int idx : opening_bracket_index) {
-            if (i < l && idx < closing_bracket_index.get(i)) {
-                i += 1;
-            }
-        }
-        return i >= 2;
+        return lst.stream().map(p -> (int) Math.ceil(p)).map(p -> p * 2).reduce(Integer::sum).get();
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.isNested("[[]]" ),
-                !s.isNested("[]]]]]]][[[[[]" ),
-                !s.isNested("[][]" ),
-                !s.isNested("[]" ),
-                s.isNested("[[]][[" ),
-                s.isNested("[[][]]" )
+                s.sumSquares(Arrays.asList(1., 2., 3.)) == 14,
+                s.sumSquares(Arrays.asList(1., 4., 9.)) == 98,
+                s.sumSquares(Arrays.asList(1., 3., 5., 7.)) == 84,
+                s.sumSquares(Arrays.asList(1.4, 4.2, 0.)) == 29,
+                s.sumSquares(Arrays.asList(-2.4, 1., 1.)) == 6
         );
         if (correct.contains(false)) {
             throw new AssertionError();
