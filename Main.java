e699a18384aa3bfc15a870541af675b35e026efe
swe-bench <>
2024-06-12 16:15:34 -0400

    Task instance Java/105

--- Main.java
+++ Main.java
@@ -3,43 +3,55 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given a list of positive integers x. return a sorted list of all
-    elements that hasn't any even digit.
+    Given an array of integers, sort the integers that are between 1 and 9 inclusive,
+    reverse the resulting array, and then replace each digit by its corresponding name from
+    "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine".
 
-    Note: Returned list should be sorted in increasing order.
-    
     For example:
-    >>> uniqueDigits(Arrays.asList(15, 33, 1422, 1))
-    [1, 15, 33]
-    >>> uniqueDigits(Arrays.asList(152, 323, 1422, 10))
-    []
-     */
-    public List<Integer> uniqueDigits(List<Integer> x) {
+      arr = [2, 1, 1, 4, 5, 8, 2, 3]
+            -> sort arr -> [1, 1, 2, 2, 3, 4, 5, 8]
+            -> reverse arr -> [8, 5, 4, 3, 2, 2, 1, 1]
+      return ["Eight", "Five", "Four", "Three", "Two", "Two", "One", "One"]
 
-        List<Integer> odd_digit_elements = new ArrayList<>();
-        for (int i : x) {
-            boolean is_unique = true;
-            for (char c : String.valueOf(i).toCharArray()) {
-                if ((c - '0') % 2 == 0) {
-                    is_unique = false;
-                    break;
+      If the array is empty, return an empty array:
+      arr = []
+      return []
+
+      If the array has any strange number ignore it:
+      arr = [1, -1 , 55]
+            -> sort arr -> [-1, 1, 55]
+            -> reverse arr -> [55, 1, -1]
+      return = ["One"]
+     */
+    public List<String> byLength(List<Integer> arr) {
+
+        List<Integer> sorted_arr = new ArrayList<>(arr);
+        List<String> new_arr = new ArrayList<>();
+        for (int var : sorted_arr) {
+            if (var >= 1 && var <= 9) {
+                switch (var) {
+                    case 1 -> new_arr.add("One");
+                    case 2 -> new_arr.add("Two");
+                    case 3 -> new_arr.add("Three");
+                    case 4 -> new_arr.add("Four");
+                    case 5 -> new_arr.add("Five");
+                    case 6 -> new_arr.add("Six");
+                    case 7 -> new_arr.add("Seven");
+                    case 8 -> new_arr.add("Eight");
+                    case 9 -> new_arr.add("Nine");
                 }
             }
-            if (is_unique) {
-                odd_digit_elements.add(i);
-                odd_digit_elements.add(1);
-            }
         }
-        Collections.sort(odd_digit_elements);
-        return odd_digit_elements;
+        return new_arr;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.uniqueDigits(Arrays.asList(15, 33, 1422, 1)).equals(Arrays.asList(1, 15, 33)),
-                s.uniqueDigits(Arrays.asList(152, 323, 1422, 10)).equals(List.of())
+                s.byLength(new ArrayList<>(Arrays.asList(2, 1, 1, 4, 5, 8, 2, 3))).equals(Arrays.asList("Eight", "Five", "Four", "Three", "Two", "Two", "One", "One" )),
+                s.byLength(new ArrayList<>(List.of())).equals(List.of()),
+                s.byLength(new ArrayList<>(Arrays.asList(1, -1, 55))).equals(List.of("One" ))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
