776686645b3c1e237c8b86b7a5e4164a033784fe
swe-bench <>
2024-06-12 16:15:27 -0400

    Task instance Java/13 with tests

---
+++ Test.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+import java.security.*;
+import java.util.*;
+import java.lang.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<Boolean> correct = Arrays.asList(
+                s.greatestCommonDivisor(3, 7) == 1,
+                s.greatestCommonDivisor(10, 15) == 5,
+                s.greatestCommonDivisor(49, 14) == 7,
+                s.greatestCommonDivisor(144, 60) == 12
+        );
+        if (correct.contains(false)) {
+            throw new AssertionError();
+        }
+    }
+}
