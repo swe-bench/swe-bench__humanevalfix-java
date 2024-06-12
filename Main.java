0e80ae0e99f0eeaf97bbe642f59156e4a13d6eb6
swe-bench <>
2024-06-12 16:15:28 -0400

    Task instance Java/33

--- Main.java
+++ Main.java
@@ -3,53 +3,33 @@ import java.lang.*;
 
 class Solution {
     /**
-    Evaluates polynomial with coefficients xs at point x.
-    return xs[0] + xs[1] * x + xs[1] * x^2 + .... xs[n] * x^n
+    This function takes a list l and returns a list l' such that
+    l' is identical to l in the indicies that are not divisible by three, while its values at the indicies that are divisible by three are equal
+    to the values of the corresponding indicies of l, but sorted.
+    >>> sortThird(Arrays.asList(1, 2, 3))
+    [1, 2, 3]
+    >>> sortThird(Arrays.asList(5, 6, 3, 4, 8, 9, 2))
+    [2, 6, 3, 4, 8, 9, 5]
      */
-    public double poly(List<Double> xs, double x) {
-        double result = 0;
-        for (int i = 0; i < xs.size(); i++) {
-            result += xs.get(i) * Math.pow(x, i);
+    public List<Integer> sortThird(List<Integer> l) {
+
+        List<Integer> thirds = new ArrayList<>();
+        for (int i = 0; i < l.size(); i += 3) {
+            thirds.add(l.get(i));
+        }
+        List<Integer> result = l;
+        for (int i = 0; i < l.size(); i += 3) {
+            result.set(i, thirds.get(i / 3));
         }
         return result;
-    }
-    
-    /**
-    xs are coefficients of a polynomial.
-    findZero find x such that poly(x) = 0.
-    findZero returns only only zero point, even if there are many.
-    Moreover, findZero only takes list xs having even number of coefficients
-    and largest non zero coefficient as it guarantees
-    a solution.
-    >>> findZero(Arrays.asList(1, 2)) // f(x) = 1 + 2x
-    -0.5
-    >>> findZero(Arrays.asList(-6, 11, -6, 1)) // (x - 1) * (x - 2) * (x - 3) = -6 + 11x - 6x^2 + x^3
-    1.0
-     */
-    public double findZero(List<Double> xs) {
-
-        double begin = -1, end = 1;
-        while (poly(xs, begin) * poly(xs, end) > 0) {
-            begin *= 2;
-            end *= 2;
-        }
-        while (begin - end  > 1e-10) {
-            double center = (begin + end) / 2;
-            if (poly(xs, begin) * poly(xs, center) > 0) {
-                begin = center;
-            } else {
-                end = center;
-            }
-        }
-        return begin;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-            Math.abs(s.findZero(new ArrayList<>(Arrays.asList(1.,2.)))+0.5)<1e-4,
-            Math.abs(s.findZero(new ArrayList<>(Arrays.asList(-6.,11.,-6.,1.)))-1)<1e-4
+            s.sortThird(new ArrayList<>(Arrays.asList(1,2,3))).equals(Arrays.asList(1,2,3)),
+            s.sortThird(new ArrayList<>(Arrays.asList(5, 6, 3, 4, 8, 9, 2))).equals(Arrays.asList(2, 6, 3, 4, 8, 9, 5))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
