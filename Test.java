cc2f99c42d3fcc8985983dae469a0b4b36d9bc35
swe-bench <>
2024-06-12 16:15:30 -0400

    Task instance Java/57 with tests

---
+++ Test.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+import java.security.*;
+import java.util.*;
+import java.lang.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<Boolean> correct = Arrays.asList(
+                s.monotonic(new ArrayList<>(Arrays.asList(1, 2, 4, 10))),
+                s.monotonic(new ArrayList<>(Arrays.asList(1, 2, 4, 20))),
+                !s.monotonic(new ArrayList<>(Arrays.asList(1, 20, 4, 10))),
+                s.monotonic(new ArrayList<>(Arrays.asList(4, 1, 0, -10))),
+                s.monotonic(new ArrayList<>(Arrays.asList(4, 1, 1, 0))),
+                !s.monotonic(new ArrayList<>(Arrays.asList(1, 2, 3, 2, 5, 60))),
+                s.monotonic(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 60))),
+                s.monotonic(new ArrayList<>(Arrays.asList(9, 9, 9, 9)))
+        );
+        if (correct.contains(false)) {
+            throw new AssertionError();
+        }
+    }
+}
