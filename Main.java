716ee53c1c5e5bcaaa8988f9a603488c8c0e810d
swe-bench <>
2024-06-12 16:15:26 -0400

    Task instance Java/8

--- Main.java
+++ Main.java
@@ -3,29 +3,31 @@ import java.lang.*;
 
 class Solution {
     /**
-    Filter an input list of strings only for ones that contain given substring
-    >>> filterBySubstring(List.of(), "a")
-    []
-    >>> filterBySubstring(Arrays.asList("abc", "bacd", "cde", "array"), "a")
-    ["abc", "bacd", "array"]
+    For a given list of integers, return a tuple consisting of a sum and a product of all the integers in a list.
+    Empty sum should be equal to 0 and empty product should be equal to 1.
+    >>> sumProduct(List.of())
+    [0, 1]
+    >>> sumProduct(Arrays.asList(1, 2, 3, 4))
+    [10, 24]
      */
-    public List<String> filterBySubstring(List<String> strings, String substring) {
+    public List<Integer> sumProduct(List<Integer> numbers) {
 
-        List<String> result = new ArrayList<>();
-        for (String x : strings) {
-            if (substring.contains(x)) {
-                result.add(x);
-            }
+        int sum = 0;
+        int product = 0;
+
+        for (int n : numbers) {
+            sum += n;
+            product *= n;
         }
-        return result;
+        return Arrays.asList(sum, product);
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.filterBySubstring(new ArrayList<>(List.of()), "s").equals(List.of()),
-                s.filterBySubstring(new ArrayList<>(Arrays.asList("abc", "bacd", "cde", "array")), "a").equals(Arrays.asList("abc", "bacd", "array"))
+                s.sumProduct(new ArrayList<>(List.of())).equals(Arrays.asList(0, 1)),
+                s.sumProduct(new ArrayList<>(Arrays.asList(1, 2, 3,4))).equals(Arrays.asList(10, 24))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
