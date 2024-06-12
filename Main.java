df1d8ce34eac4d64b1b5df14a7e7ea61f9d679e7
swe-bench <>
2024-06-12 16:15:32 -0400

    Task instance Java/87

--- Main.java
+++ Main.java
@@ -3,36 +3,50 @@ import java.lang.*;
 
 class Solution {
     /**
-    Write a function that takes a string and returns an ordered version of it.
-    Ordered version of string, is a string where all words (separated by space)
-    are replaced by a new word where all the characters arranged in
-    ascending order based on ascii value.
-    Note: You should keep the order of words and blank spaces in the sentence.
+    You are given a 2 dimensional data, as a nested lists,
+    which is similar to matrix, however, unlike matrices,
+    each row may contain a different number of columns.
+    Given lst, and integer x, find integers x in the list,
+    and return list of lists, [[x1, y1], [x2, y2] ...] such that
+    each list is a coordinate - (row, columns), starting with 0.
+    Sort coordinates initially by rows in ascending order.
+    Also, sort coordinates of the row by columns in descending order.
 
-    For example:
-    antiShuffle("Hi") returns "Hi"
-    antiShuffle("hello") returns "ehllo"
-    antiShuffle("Hello World!!!") returns "Hello !!!Wdlor"
+    Examples:
+    getRow([
+      [1,2,3,4,5,6],
+      [1,2,3,4,1,6],
+      [1,2,3,4,5,1]
+    ], 1) == [[0, 0], [1, 4], [1, 0], [2, 5], [2, 0]]
+    getRow([], 1) == []
+    getRow([[], [1], [1, 2, 3]], 3) == [[2, 2]]
      */
-    public String antiShuffle(String s) {
+    public List<List<Integer>> getRow(List<List<Integer>> lst, int x) {
 
-        String[] strings = s.split(" ");
-        List<String> result = new ArrayList<>();
-        for (String string : strings) {
-            char[] chars = string.toCharArray();
-            Arrays.sort(chars);
-            result.add(String.copyValueOf(chars));
+        List<List<Integer>> coords = new ArrayList<>();
+        for (int i = 0; i < lst.size(); i++) {
+            List<List<Integer>> row = new ArrayList<>();
+            for (int j = lst.get(i).size() - 1; j >= 0; j--) {
+                if (lst.get(i).get(j) == x) {
+                    row.add(Arrays.asList(j, i));
+                }
+            }
+            coords.addAll(row);
         }
-        return String.join("", result);
+        return coords;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                Objects.equals(s.antiShuffle("Hi"), "Hi"),
-                Objects.equals(s.antiShuffle("hello"), "ehllo"),
-                Objects.equals(s.antiShuffle("Hello World!!!"), "Hello !!!Wdlor")
+                s.getRow(Arrays.asList(
+                        Arrays.asList(1, 2, 3, 4, 5, 6),
+                        Arrays.asList(1, 2, 3, 4, 1, 6),
+                        Arrays.asList(1, 2, 3, 4, 5, 1)
+                ), 1).equals(Arrays.asList(Arrays.asList(0, 0), Arrays.asList(1, 4), Arrays.asList(1, 0), Arrays.asList(2, 5), Arrays.asList(2, 0))),
+                s.getRow(List.of(), 1).equals(List.of()),
+                s.getRow(Arrays.asList(List.of(), List.of(1), Arrays.asList(1, 2, 3)), 3).equals(List.of(Arrays.asList(2, 2)))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
