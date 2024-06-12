6d9cfe7442454885e594b84e7420d8e7f82d1b04
swe-bench <>
2024-06-12 16:15:28 -0400

    Task instance Java/38

--- Main.java
+++ Main.java
@@ -3,37 +3,28 @@ import java.lang.*;
 
 class Solution {
     /**
-    This function takes a list l and returns a list l' such that
-    l' is identical to l in the odd indicies, while its values at the even indicies are equal
-    to the values of the even indicies of l, but sorted.
-    >>> sortEven(Arrays.asList(1, 2, 3))
-    [1, 2, 3]
-    >>> sortEven(Arrays.asList(5, 6, 3, 4))
-    [3, 6, 5, 4]
+    returns encoded string by cycling groups of three characters.
      */
-    public List<Integer> sortEven(List<Integer> l) {
-
-        List<Integer> even = new ArrayList<>();
-        for (int i = 0; i < l.size(); i += 2) {
-            even.add(l.get(i));
+    public String encodeCyclic(String s) {
+        // split string to groups. Each of length 3.
+        List<String> groups = new ArrayList<>();
+        for (int i = 0; i < s.length(); i += 3) {
+            groups.add(s.substring(i, Math.min(i + 3, s.length())));
         }
-        Collections.sort(l);
-        List<Integer> result = l;
-        for (int i = 0; i < l.size(); i += 2) {
-            result.set(i, even.get(i / 2));
+        // cycle elements in each group. Unless group has fewer elements than 3.
+        for (int i = 0; i < groups.size(); i++) {
+            if (groups.get(i).length() == 3) {
+                groups.set(i, groups.get(i).substring(1) + groups.get(i).charAt(0));
+            }
         }
-        return result;
+        return String.join("", groups);
     }
-}
-public class Main {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        List<Boolean> correct = Arrays.asList(
-                s.sortEven(new ArrayList<>(Arrays.asList(1, 2, 3))).equals(Arrays.asList(1, 2, 3)),
-                s.sortEven(new ArrayList<>(Arrays.asList(5,6,3,4))).equals(Arrays.asList(3,6,5,4))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+
+    /**
+    takes as input string encoded with encodeCyclic function. Returns decoded string.
+     */
+    public String decodeCyclic(String s) {
+
+        return encodeCyclic(s);
     }
 }
