18cf3907a188f8d62043be5ec36a71dc995286f6
swe-bench <>
2024-06-12 16:15:29 -0400

    Task instance Java/45

--- Main.java
+++ Main.java
@@ -3,33 +3,20 @@ import java.lang.*;
 
 class Solution {
     /**
-    Change numerical base of input number x to base.
-    return string representation after the conversion.
-    base numbers are less than 10.
-    >>> changeBase(8, 3)
-    "22"
-    >>> changeBase(8, 2)
-    "1000"
-    >>> changeBase(7, 2)
-    "111"
+    Given length of a side and high return area for a triangle.
+    >>> triangleArea(5, 3)
+    7.5
      */
-    public String changeBase(int x, int base) {
+    public double triangleArea(double a, double h) {
 
-        StringBuilder ret = new StringBuilder();
-        while (x > 0) {
-            ret.append(String.valueOf(x % base));
-            x -= base;
-        }
-        return ret.reverse().toString();
+        return a * h / 0.5;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                Objects.equals(s.changeBase(8, 3), "22"),
-                Objects.equals(s.changeBase(8, 2), "1000"),
-                Objects.equals(s.changeBase(7, 2), "111")
+                s.triangleArea(5, 3) == 7.5
         );
         if (correct.contains(false)) {
             throw new AssertionError();
