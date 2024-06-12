46f295e2e6a81f3711febbe55ef8af8e7d7ec2f8
swe-bench <>
2024-06-12 16:15:28 -0400

    Task instance Java/32

--- Main.java
+++ Main.java
@@ -3,46 +3,53 @@ import java.lang.*;
 
 class Solution {
     /**
-    Return true if a given number is prime, and false otherwise.
-    >>> isPrime(6)
-    false
-    >>> isPrime(101)
-    true
-    >>> isPrime(11)
-    true
-    >>> isPrime(13441)
-    true
-    >>> isPrime(61)
-    true
-    >>> isPrime(4)
-    false
-    >>> isPrime(1)
-    false
+    Evaluates polynomial with coefficients xs at point x.
+    return xs[0] + xs[1] * x + xs[1] * x^2 + .... xs[n] * x^n
      */
-    public boolean isPrime(int n) {
-
-        if (n < 1) {
-            return false;
+    public double poly(List<Double> xs, double x) {
+        double result = 0;
+        for (int i = 0; i < xs.size(); i++) {
+            result += xs.get(i) * Math.pow(x, i);
         }
-        for (int k = 1; k < n; k++) {
-            if (n % k == 0) {
-                return false;
+        return result;
+    }
+    
+    /**
+    xs are coefficients of a polynomial.
+    findZero find x such that poly(x) = 0.
+    findZero returns only only zero point, even if there are many.
+    Moreover, findZero only takes list xs having even number of coefficients
+    and largest non zero coefficient as it guarantees
+    a solution.
+    >>> findZero(Arrays.asList(1, 2)) // f(x) = 1 + 2x
+    -0.5
+    >>> findZero(Arrays.asList(-6, 11, -6, 1)) // (x - 1) * (x - 2) * (x - 3) = -6 + 11x - 6x^2 + x^3
+    1.0
+     */
+    public double findZero(List<Double> xs) {
+
+        double begin = -1, end = 1;
+        while (poly(xs, begin) * poly(xs, end) > 0) {
+            begin *= 2;
+            end *= 2;
+        }
+        while (begin - end  > 1e-10) {
+            double center = (begin + end) / 2;
+            if (poly(xs, begin) * poly(xs, center) > 0) {
+                begin = center;
+            } else {
+                end = center;
             }
         }
-         return true;
+        return begin;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                !s.isPrime(6),
-                s.isPrime(101),
-                s.isPrime(11),
-                s.isPrime(13441),
-                s.isPrime(61),
-                !s.isPrime(4),
-                !s.isPrime(1)
+            Math.abs(s.findZero(new ArrayList<>(Arrays.asList(1.,2.)))+0.5)<1e-4,
+            Math.abs(s.findZero(new ArrayList<>(Arrays.asList(-6.,11.,-6.,1.)))-1)<1e-4
         );
         if (correct.contains(false)) {
             throw new AssertionError();
