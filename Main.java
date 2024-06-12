95f4c6e8656079f950a8c9271cbed6c972867ea3
swe-bench <>
2024-06-12 16:15:31 -0400

    Task instance Java/74

--- Main.java
+++ Main.java
@@ -3,34 +3,46 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given an array arr of integers, find the minimum number of elements that
-    need to be changed to make the array palindromic. A palindromic array is an array that
-    is read the same backwards and forwards. In one change, you can change one element to any other element.
+    Write a function that accepts two lists of strings and returns the list that has
+    total number of chars in the all strings of the list less than the other list.
 
-    For example:
-    smallestChange(Arrays.asList(1,2,3,5,4,7,9,6)) == 4
-    smallestChange(Arrays.asList(1, 2, 3, 4, 3, 2, 2)) == 1
-    smallestChange(Arrays.asList(1, 2, 3, 2, 1)) == 0
+    if the two lists have the same number of chars, return the first list.
+
+    Examples
+    totalMatch(Arrays.asList(), Arrays.asList()) -> []
+    totalMatch(Arrays.asList("hi", "admin"), Arrays.asList("hI", "Hi")) -> ["hI", "Hi"]
+    totalMatch(Arrays.asList("hi", "admin"), Arrays.asList("hi", "hi", "admin", "project")) -> ["hi", "admin"]
+    totalMatch(Arrays.asList("hi", "admin"), Arrays.asList("hI", "hi", "hi")) -> ["hI", "hi", "hi"]
+    totalMatch(Arrays.asList("4"), Arrays.asList("1", "2", "3", "4", "5")) -> ["4"]
      */
-    public int smallestChange(List<Integer> arr) {
+    public List<String> totalMatch(List<String> lst1, List<String> lst2) {
 
-        int ans = 0;
-        for (int i = 0; i < arr.size() / 2; i++) {
-            if (!Objects.equals(ans, arr.get(arr.size() - i - 1))) {
-                ans += 1;
-            }
+        int l1 = 0;
+        for (String st : lst1) {
+            l1 += st.length();
         }
-        return ans;
+
+        int l2 = 0;
+        for (String st : lst2) {
+            l2 += st.length();
+        }
+
+        if (l1 <= l2) {
+            return lst2;
+        } else {
+            return lst1;
+        }
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.smallestChange(new ArrayList<>(Arrays.asList(1, 2, 3, 5, 4, 7, 9, 6))) == 4,
-                s.smallestChange(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 3, 2, 2))) == 1,
-                s.smallestChange(new ArrayList<>(Arrays.asList(1, 2, 3, 2, 1))) == 0,
-                s.smallestChange(new ArrayList<>(Arrays.asList(3, 1, 1, 3))) == 0
+                s.totalMatch(new ArrayList<>(List.of()), new ArrayList<>(List.of())).equals(List.of()),
+                s.totalMatch(new ArrayList<>(Arrays.asList("hi", "admin")), new ArrayList<>(Arrays.asList("hi", "hi", "admin", "project"))).equals(Arrays.asList("hi", "admin")),
+                s.totalMatch(new ArrayList<>(List.of("4")), new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5"))).equals(List.of("4")),
+                s.totalMatch(new ArrayList<>(Arrays.asList("hi", "admin")), new ArrayList<>(Arrays.asList("hI", "Hi"))).equals(Arrays.asList("hI", "Hi")),
+                s.totalMatch(new ArrayList<>(Arrays.asList("hi", "admin")), new ArrayList<>(Arrays.asList("hI", "hi", "hi"))).equals(Arrays.asList("hI", "hi", "hi"))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
