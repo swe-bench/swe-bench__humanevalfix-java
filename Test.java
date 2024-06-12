c70989b721e5b4a60351f75c5c3ecd6fcb58cb3b
swe-bench <>
2024-06-12 16:15:27 -0400

    Task instance Java/14 with tests

---
+++ Test.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.security.*;
+import java.util.*;
+import java.lang.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<Boolean> correct = Arrays.asList(
+                s.allPrefixes("").equals(List.of()),
+                s.allPrefixes("asdfgh").equals(Arrays.asList("a", "as", "asd", "asdf", "asdfg", "asdfgh")),
+                s.allPrefixes("WWW").equals(Arrays.asList("W", "WW", "WWW"))
+        );
+        if (correct.contains(false)) {
+            throw new AssertionError();
+        }
+    }
+}
