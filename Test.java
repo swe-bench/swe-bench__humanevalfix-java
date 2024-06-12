d2973a8785ffba5194ad01dd6e671c9a4700f500
swe-bench <>
2024-06-12 16:15:29 -0400

    Task instance Java/47 with tests

---
+++ Test.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+import java.security.*;
+import java.util.*;
+import java.lang.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<Boolean> correct = Arrays.asList(
+                s.median(new ArrayList<>(Arrays.asList(3, 1, 2, 4, 5))) == 3,
+                s.median(new ArrayList<>(Arrays.asList(-10, 4, 6, 1000, 10, 20))) == 8.0,
+                s.median(new ArrayList<>(Arrays.asList(5))) == 5,
+                s.median(new ArrayList<>(Arrays.asList(6, 5))) == 5.5
+        );
+        if (correct.contains(false)) {
+            throw new AssertionError();
+        }
+    }
+}
