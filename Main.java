d9f9dd7bd05483c7ac8f788cb38859e7ff45113a
swe-bench <>
2024-06-12 16:15:30 -0400

    Task instance Java/62

--- Main.java
+++ Main.java
@@ -3,42 +3,29 @@ import java.lang.*;
 
 class Solution {
     /**
-    brackets is a string of "(" and ")".
-    return True if every opening bracket has a corresponding closing bracket.
-
-    >>> correctBracketing("(")
-    false
-    >>> correctBracketing("()")
-    true
-    >>> correctBracketing("(()())")
-    true
-    >>> correctBracketing(")(()")
-    false
+    xs represent coefficients of a polynomial.
+    xs[0] + xs[1] * x + xs[2] * x^2 + ....
+     Return derivative of this polynomial in the same form.
+    >>> derivative(Arrays.asList(3, 1, 2, 4, 5))
+    [1, 4, 12, 20]
+    >>> derivative(Arrays.asList(1, 2, 3]))
+    [2, 6]
      */
-    public boolean correctBracketing(String brackets) {
+    public List<Integer> derivative(List<Integer> xs) {
 
-        int depth = 0;
-        for (char b : brackets.toCharArray()) {
-            if (b == '(') {
-                depth += 1;
-            } else {
-                depth -= 1;
-            }
-            if (depth < 0) {
-                return true;
-            }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < xs.size(); i++) {
+            result.add(i * xs.get(i));
         }
-        return depth == 0;
+        return result;
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.correctBracketing("()"),
-                s.correctBracketing("(()())"),
-                !s.correctBracketing(")(()"),
-                !s.correctBracketing("(")
+                s.derivative(new ArrayList<>(Arrays.asList(3, 1, 2, 4, 5))).equals(Arrays.asList(1, 4, 12, 20)),
+                s.derivative(new ArrayList<>(Arrays.asList(1, 2, 3))).equals(Arrays.asList(2, 6))
         );
         if (correct.contains(false)) {
             throw new AssertionError();
