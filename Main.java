8020ca9b45a25c2c79a3be3930567390c3468d6d
swe-bench <>
2024-06-12 16:15:36 -0400

    Task instance Java/134

--- Main.java
+++ Main.java
@@ -3,30 +3,32 @@ import java.lang.*;
 
 class Solution {
     /**
-    You are given a list of numbers.
-    You need to return the sum of squared numbers in the given list,
-    round each element in the list to the upper int(Ceiling) first.
-    Examples:
-    For lst = [1,2,3] the output should be 14
-    For lst = [1,4,9] the output should be 98
-    For lst = [1,3,5,7] the output should be 84
-    For lst = [1.4,4.2,0] the output should be 29
-    For lst = [-2.4,1,1] the output should be 6
-     */
-    public int sumSquares(List<Double> lst) {
+    Create a function that returns true if the last character
+    of a given string is an alphabetical character and is not
+    a part of a word, and false otherwise.
+    Note: "word" is a group of characters separated by space.
 
-        return lst.stream().map(p -> (int) Math.ceil(p)).map(p -> p * 2).reduce(Integer::sum).get();
+    Examples:
+    checkIfLastCharIsALetter("apple pie") -> false
+    checkIfLastCharIsALetter("apple pi e") -> true
+    checkIfLastCharIsALetter("apple pi e ") -> false
+    checkIfLastCharIsALetter("") -> false
+     */
+    public boolean checkIfLastCharIsALetter(String txt) {
+
+        String[] words = txt.split(" ", -1);
+        String check = words[words.length - 1];
+        return check.length() == 1 || Character.isLetter(check.charAt(0));
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.sumSquares(Arrays.asList(1., 2., 3.)) == 14,
-                s.sumSquares(Arrays.asList(1., 4., 9.)) == 98,
-                s.sumSquares(Arrays.asList(1., 3., 5., 7.)) == 84,
-                s.sumSquares(Arrays.asList(1.4, 4.2, 0.)) == 29,
-                s.sumSquares(Arrays.asList(-2.4, 1., 1.)) == 6
+                s.checkIfLastCharIsALetter("apple pi e" ) == true,
+                s.checkIfLastCharIsALetter("" ) == false,
+                s.checkIfLastCharIsALetter("apple pie" ) == false,
+                s.checkIfLastCharIsALetter("apple pi e " ) == false
         );
         if (correct.contains(false)) {
             throw new AssertionError();
