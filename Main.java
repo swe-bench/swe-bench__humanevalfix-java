2b70f60c184eb2b89238d782e2440bb7fee2d805
swe-bench <>
2024-06-12 16:15:35 -0400

    Task instance Java/118

--- Main.java
+++ Main.java
@@ -3,45 +3,43 @@ import java.lang.*;
 
 class Solution {
     /**
-    Given a string s and a natural number n, you have been tasked to implement
-    a function that returns a list of all words from string s that contain exactly
-    n consonants, in order these words appear in the string s.
-    If the string s is empty then the function should return an empty list.
-    Note: you may assume the input string contains only letters and spaces.
-    Examples:
-    selectWords("Mary had a little lamb", 4) ==> ["little"]
-    selectWords("Mary had a little lamb", 3) ==> ["Mary", "lamb"]
-    selectWords("simple white space", 2) ==> []
-    selectWords("Hello world", 4) ==> ["world"]
-    selectWords("Uncle sam", 3) ==> ["Uncle"]
-     */
-    public List<String> selectWords(String s, int n) {
+    You are given a word. Your task is to find the closest vowel that stands between
+    two consonants from the right side of the word (case sensitive).
 
-        List<String> result = new ArrayList<>();
-        for (String word : s.split(" ")) {
-            int n_consonants = 0;
-            for (char c : word.toCharArray()) {
-                c = Character.toLowerCase(c);
-                if ("aeiou".indexOf(c) != -1) {
-                    n_consonants += 1;
-                }
-            }
-            if (n_consonants == n) {
-                result.add(word);
+    Vowels in the beginning and ending doesn't count. Return empty string if you didn't
+    find any vowel met the above condition.
+
+    You may assume that the given string contains English letter only.
+
+    Example:
+    getClosestVowel("yogurt") ==> "u"
+    getClosestVowel("FULL") ==> "U"
+    getClosestVowel("quick") ==> ""
+    getClosestVowel("ab") ==> ""
+     */
+    public String getClosestVowel(String word) {
+
+        if (word.length() < 3) {
+            return " ";
+        }
+
+        String vowels = "aeiouAEIOU";
+        for (int i = word.length() - 2; i > 0; i--) {
+            if (vowels.indexOf(word.charAt(i)) != -1 && vowels.indexOf(word.charAt(i + 1)) == -1 && vowels.indexOf(word.charAt(i - 1)) == -1) {
+                return String.valueOf(word.charAt(i));
             }
         }
-        return result;
+        return " ";
     }
 }
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         List<Boolean> correct = Arrays.asList(
-                s.selectWords("Mary had a little lamb", 4).equals(List.of("little" )),
-                s.selectWords("Mary had a little lamb", 3).equals(Arrays.asList("Mary", "lamb")),
-                s.selectWords("simple white space", 2).equals(List.of()),
-                s.selectWords("Hello world", 4).equals(List.of("world" )),
-                s.selectWords("Uncle sam", 3).equals(List.of("Uncle" ))
+                s.getClosestVowel("yogurt").equals("u"),
+                s.getClosestVowel("FULL").equals("U"),
+                s.getClosestVowel("ab").equals(""),
+                s.getClosestVowel("quick").equals("")
         );
         if (correct.contains(false)) {
             throw new AssertionError();
